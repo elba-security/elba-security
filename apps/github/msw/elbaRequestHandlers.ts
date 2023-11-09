e9d4b87151c4082f2ffc4e2f05d4e14c073683a5
Alexandre Nicolas <alexandre@elba.security>
2023-11-09 15:18:57 +0100

    refactor(github): implement template

--- apps/github/msw/elbaRequestHandlers.ts
+++ apps/github/msw/elbaRequestHandlers.ts
@@ -3,7 +3,7 @@ import {
   RequestHandlerDefaultInfo,
   RequestHandlerOptions,
 } from 'msw/lib/core/handlers/RequestHandler';
-import { env } from '../env';
+import { env } from '../../g2/src/common/env';
 import { z } from 'zod';
 
 const updateUsersSchema = z.object({
