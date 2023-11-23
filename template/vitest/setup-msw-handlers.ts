8a86959f74746bb4d8ffd378ca22d5af57a45d3c
Alexandre Nicolas <alexandre@elba.security>
2023-11-23 15:20:34 +0100

    chore(template): add drizzle required configuration & docs

---
+++ template/vitest/setup-msw-handlers.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment -- conveniency until we enforce an env. var. validation */
+import { createElbaRequestHandlers } from 'elba-msw';
+import { setupServer } from 'msw/node';
+import { beforeAll, afterAll, afterEach } from 'vitest';
+
+const elbaRequestHandlers = createElbaRequestHandlers(
+  process.env.ELBA_API_BASE_URL!,
+  process.env.ELBA_API_KEY!
+);
+
+const server = setupServer(...elbaRequestHandlers);
+
+beforeAll(() => {
+  server.listen({ onUnhandledRequest: 'error' });
+});
+afterAll(() => {
+  server.close();
+});
+afterEach(() => {
+  server.resetHandlers();
+});
