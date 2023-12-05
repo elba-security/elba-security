4fb8fec1fe174e0322de50fe64cdd068413c8784
Alexandre Nicolas <alexandre@elba.security>
2023-12-05 16:02:50 +0100

    feat(template): add env vars zod validation

---
+++ template/src/env.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod';
+
+export const env = z
+  .object({
+    ELBA_API_KEY: z.string().min(1),
+    ELBA_API_BASE_URL: z.string().url(),
+    ELBA_REDIRECT_URL: z.string().url(),
+    ELBA_SOURCE_ID: z.string().uuid(),
+    POSTGRES_URL: z.string().min(1),
+    POSTGRES_HOST: z.string().min(1),
+    POSTGRES_PORT: z.coerce.number().int().positive(),
+    POSTGRES_USER: z.string().min(1),
+    POSTGRES_PASSWORD: z.string().min(1),
+    POSTGRES_DATABASE: z.string().min(1),
+    POSTGRES_PROXY: z.coerce.number().int().positive(),
+  })
+  .parse(process.env);
