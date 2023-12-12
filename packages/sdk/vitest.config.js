ee149c437598a86a20b2d5b66c62cfe9b868e2ce
Alexandre Nicolas <alexandre@elba.security>
2023-12-12 11:08:52 +0100

    chore(sdk): use vitest edge-runtime environment

--- packages/sdk/vitest.config.js
+++ packages/sdk/vitest.config.js
@@ -3,7 +3,8 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    setupFiles: ['./vitest/setup-test-server.ts', './vitest/setup-crypto-polyfill.ts'],
+    setupFiles: ['./vitest/setup-test-server.ts'],
+    environment: ['edge-runtime'],
     env: {
       ELBA_API_BASE_URL: 'http://foo.bar',
       ELBA_API_KEY: 'some-api-key',
