10dddfc0fbcbff5136d3a54355f3f45298fa779a
Alexandre Nicolas <alexandre@elba.security>
2023-12-14 13:43:16 +0100

    feat(schemas): add some webhook event data schemas

--- packages/schemas/src/index.ts
+++ packages/schemas/src/index.ts
@@ -2,5 +2,6 @@ export * from './authentication';
 export * from './common';
 export * from './connection-status';
 export * from './data-protection';
+export * from './miscellaneous';
 export * from './third-party-apps';
 export * from './users';
