d8af1a5c0c216b5875208d74f4e0d6ce70caefc5
Maksym <148101061+mbliakharskyi@users.noreply.github.com>
2023-12-18 12:34:20 +0200

    Update apps/asana/src/database/schema.ts
    Co-authored-by: Alexandre Nicolas <117630727+elbalexandre@users.noreply.github.com>

--- apps/asana/src/database/schema.ts
+++ apps/asana/src/database/schema.ts
@@ -4,7 +4,6 @@ import { type InferSelectModel } from 'drizzle-orm';
 export const Organisation = pgTable('organisation', {
   id: uuid('id').primaryKey(),
   name: text('name'),
-  email: text('email').notNull(),
   accessToken: text('access_token').notNull(),
   refreshToken: text('refresh_token').notNull(),
   expiresAt: timestamp('expires_at').notNull(),
