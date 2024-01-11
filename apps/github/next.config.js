330c26a88dbed6aa89b0114988ce797568ec2bf4
Alexandre Nicolas <alexandre@elba.security>
2024-01-11 15:30:49 +0100

    chore(github): apply review

--- apps/github/next.config.js
+++ apps/github/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['elba-sdk'],
+  transpilePackages: ['@elba-security/sdk'],
 };
 
 module.exports = nextConfig;
--- apps/github/src/app/install/route.ts
+++ apps/github/src/app/install/route.ts
@@ -9,13 +9,11 @@ export function GET(request: NextRequest) {
   const organisationId = request.nextUrl.searchParams.get('organisation_id');
   const region = request.nextUrl.searchParams.get('region');
 
-  if (!organisationId) {
-    redirect(`${env.ELBA_REDIRECT_URL}?error=true`);
+  if (!organisationId || !region) {
+    redirect(`${env.ELBA_REDIRECT_URL}?error=internal_error`);
   }
 
   cookies().set('organisation_id', organisationId);
-  if (region) {
-    cookies().set('region', region);
-  }
+  cookies().set('region', region);
   redirect(env.GITHUB_APP_INSTALL_URL);
 }
