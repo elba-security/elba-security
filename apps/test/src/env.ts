e49893abc255626e98ea9df85f5f1977ffc79e1e
Guillaume PROQUIN <guillaume.proquin@epitech.eu>
2024-01-11 16:53:46 +0100

    chore: remove test app

--- apps/test/src/env.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const env = z
-  .object({
-    VERCEL_PREFERRED_REGION: z.string().min(1),
-    VERCEL_ENV: z.string().min(1).optional(),
-  })
-  .parse(process.env);
