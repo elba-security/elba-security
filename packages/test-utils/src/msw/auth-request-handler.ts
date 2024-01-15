2a9a45f4ddfc1471bfb4f082c1ea326ef2c456ab
Alexandre Nicolas <alexandre@elba.security>
2024-01-15 17:49:48 +0100

    refactor(sdk): remove authorization header

--- packages/test-utils/src/msw/auth-request-handler.ts
+++ packages/test-utils/src/msw/auth-request-handler.ts
@@ -2,6 +2,8 @@ import { type RequestHandler, http } from 'msw';
 
 export const createAuthRequestHandler = (baseUrl: string, apiKey: string): RequestHandler =>
   http.all(`${baseUrl}/*`, ({ request }) => {
+    // TODO: add authorization check
+    return;
     if (request.headers.get('Authorization') !== `Bearer ${apiKey}`) {
       return new Response(null, {
         status: 401,
