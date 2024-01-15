2e9253330b8d960700a6bc6b367f140bbac2a413
Alexandre Nicolas <alexandre@elba.security>
2024-01-15 17:59:16 +0100

    fixup! fix(github): missing source_id in elba redirect url

--- packages/test-utils/src/msw/auth-request-handler.ts
+++ packages/test-utils/src/msw/auth-request-handler.ts
@@ -1,13 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars -- convenience */
 import { type RequestHandler, http } from 'msw';
 
 export const createAuthRequestHandler = (baseUrl: string, apiKey: string): RequestHandler =>
   http.all(`${baseUrl}/*`, ({ request }) => {
     // TODO: add authorization check
-    return;
-    if (request.headers.get('Authorization') !== `Bearer ${apiKey}`) {
-      return new Response(null, {
-        status: 401,
-        statusText: 'Unauthorized',
-      });
-    }
+    // if (request.headers.get('Authorization') !== `Bearer ${apiKey}`) {
+    //   return new Response(null, {
+    //     status: 401,
+    //     statusText: 'Unauthorized',
+    //   });
+    // }
   });
