99390bde42082fd103c134760cc265bb2b4ef0dd
Alexandre Nicolas <alexandre@elba.security>
2024-02-06 11:02:27 +0100

    chore(microsoft): add middleware logs

--- apps/microsoft/src/middleware.ts
+++ apps/microsoft/src/middleware.ts
@@ -1,13 +1,14 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { validateWebhookRequestSignature } from '@elba-security/sdk';
+import { logger } from '@elba-security/logger';
 import { env } from '@/env';
 
 export async function middleware(request: NextRequest) {
   try {
     await validateWebhookRequestSignature(request, env.ELBA_WEBHOOK_SECRET);
   } catch (error) {
-    // TODO: add log
+    logger.error('Could not validate webhook request signature', { error, request });
     return new NextResponse(null, { status: 401, statusText: 'unauthorized' });
   }
 }
