99fc7f01c6e4429e3d88f20e47976dee833245b8
Alexandre Nicolas <alexandre@elba.security>
2024-02-06 10:46:23 +0100

    chore: migrate to neon connector

--- apps/microsoft/src/database/client.ts
+++ apps/microsoft/src/database/client.ts
@@ -1,11 +1,19 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
+import { Pool, neonConfig } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-serverless';
 import { env } from '@/env';
+import * as schema from './schema';
 
-const queryClient = postgres(
-  env.VERCEL_ENV && env.VERCEL_ENV !== 'development'
-    ? `${env.POSTGRES_URL}?sslmode=require`
-    : env.POSTGRES_URL
-);
+// To have a local neon database like environment as vercel postgres use neon
+// see: https://gal.hagever.com/posts/running-vercel-postgres-locally
+if (!env.VERCEL_ENV || env.VERCEL_ENV === 'development') {
+  // Set the WebSocket proxy to work with the local instance
+  neonConfig.wsProxy = (host) => `${host}:${env.DATABASE_PROXY_PORT}/v1`;
+  // Disable all authentication and encryption
+  neonConfig.useSecureWebSocket = false;
+  neonConfig.pipelineTLS = false;
+  neonConfig.pipelineConnect = false;
+}
 
-export const db = drizzle(queryClient);
+const pool = new Pool({ connectionString: env.DATABASE_URL });
+
+export const db = drizzle(pool, { schema });
