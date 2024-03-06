42663596d229a139d5eb0e9e3029f824e8aaa2fe
Ahmed Hashir <124239470+AhmedHashir10@users.noreply.github.com>
2024-03-06 19:38:12 +0500

    Implemented the remove organisation and it's test

--- apps/calendly/src/inngest/client.ts
+++ apps/calendly/src/inngest/client.ts
@@ -1,13 +1,27 @@
 import { EventSchemas, Inngest } from 'inngest';
+import { sentryMiddleware } from '@elba-security/inngest';
 import { logger } from '@elba-security/logger';
-import { rateLimitMiddleware } from '@/inngest/middlewares/rate-limit-middleware';
-import { type InngestEvents } from './types';
-
-export type FunctionHandler = Parameters<typeof inngest.createFunction>[2];
+import { rateLimitMiddleware } from './middlewares/rate-limit-middleware';
 
 export const inngest = new Inngest({
-  id: 'calendly',
-  schemas: new EventSchemas().fromRecord<InngestEvents>(),
-  middleware: [rateLimitMiddleware],
+  id: 'calendly', 
+  schemas: new EventSchemas().fromRecord<{
+    'calendly/organization.members.page_sync.requested': {
+      data: {
+        id: string;
+        accessToken: string;
+        refreshToken: string;
+        region: string;
+        createdAt: number;
+        updatedAt: number;
+      };
+    };
+    'calendly/calendly.elba_app.uninstalled': {
+      data: {
+        organizationId: string;
+      };
+    };
+  }>(),
+  middleware: [rateLimitMiddleware, sentryMiddleware], 
   logger,
 });
