452f8f35ca2e828cbff85acd0903827548136cfb
Abdullah Awan <abdullahawan7676@gmail.com>
2024-03-10 11:09:43 +0500

    feat: inngest functions added

--- apps/calendly/src/inngest/functions/index.ts
+++ apps/calendly/src/inngest/functions/index.ts
@@ -1,4 +1,13 @@
+import { removeOrganisation } from './organisation/remove-organisation';
 import { syncUsersPage } from './users/sync-users-page';
+import { scheduleUsersSyncs } from './users/schedule-users-syncs';
+import { deleteCalendlyUser } from './users/delete-user';
 import { refreshToken } from './tokens/refresh-token';
 
-export const inngestFunctions = [syncUsersPage, refreshToken];
+export const inngestFunctions = [
+  syncUsersPage,
+  scheduleUsersSyncs,
+  deleteCalendlyUser,
+  refreshToken,
+  removeOrganisation,
+];
