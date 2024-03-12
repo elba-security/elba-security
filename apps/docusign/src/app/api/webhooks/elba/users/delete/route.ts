3956019ef2a244a7721c60a1e058cf0236bd01a0
Maksym <148101061+mbliakharskyi@users.noreply.github.com>
2024-03-12 13:07:16 +0200

    Update route.ts

--- apps/docusign/src/app/api/webhooks/elba/users/delete/route.ts
+++ apps/docusign/src/app/api/webhooks/elba/users/delete/route.ts
@@ -1,17 +1,10 @@
 import type { NextRequest } from 'next/server';
+import { parseWebhookEventData } from '@elba-security/sdk';
 import { deleteUsers } from './service';
 
-type RequestParams= {
-  userId: string;
-}
 export async function POST(request: NextRequest) {
-  const data: unknown = await request.json()
-  const { userId } = parseWebhookEventData(
-    'users.delete_user_requested,
-    data
-  );
-
-  const userId  = data.userId;
+  const data: unknown = await request.json();
+  const { userId } = parseWebhookEventData(`users.delete_user_requested`, data);
 
   await deleteUsers({ userId });
 }
