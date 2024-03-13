9e5924d3b24cfc31bfc94ab08ee9101eb91bb48e
Abdullah Awan <abdullahawan7676@gmail.com>
2024-03-13 19:22:54 +0500

    feat: connector functions and tests implemented

--- apps/heroku/src/app/oauth/service.ts
+++ apps/heroku/src/app/oauth/service.ts
@@ -1,9 +1,8 @@
 import { addSeconds } from 'date-fns';
-import { getAccessToken, refreshAccessToken } from '@/connectors/auth';
+import { getAccessToken } from '@/connectors/auth';
 import { db, Organisation } from '@/database';
 import { inngest } from '@/inngest/client';
-
-
+import { getTeamId } from '@/connectors/teams';
 
 type SetupOrganisationParams = {
   organisationId: string;
@@ -17,47 +16,48 @@ export const setupOrganisation = async ({
   region,
 }: SetupOrganisationParams) => {
   const { accessToken, refreshToken, expiresIn } = await getAccessToken(code);
- 
-  
-  // const [organisation] = await db
-  //   .insert(Organisation)
-  //   .values({
-  //     id: organisationId,
-  //     accessToken,
-  //     refreshToken,
-  //     teamId: team,
-  //     region,
-  //   })
-  //   .onConflictDoUpdate({
-  //     target: [Organisation.id],
-  //     set: {
-  //       id: organisationId,
-  //       accessToken,
-  //       refreshToken,
-  //       teamId: team,
-  //       region,
-  //     },
-  //   })
-  //   .returning();
+  const teamId = await getTeamId(accessToken);
+  if (teamId) {
+    const [organisation] = await db
+      .insert(Organisation)
+      .values({
+        id: organisationId,
+        accessToken,
+        refreshToken,
+        teamId,
+        region,
+      })
+      .onConflictDoUpdate({
+        target: [Organisation.id],
+        set: {
+          id: organisationId,
+          accessToken,
+          refreshToken,
+          teamId,
+          region,
+        },
+      })
+      .returning();
 
-  // await inngest.send([
-  //   {
-  //     name: 'heroku/token.refresh.requested',
-  //     data: {
-  //       organisationId,
-  //       expiresAt: addSeconds(new Date(), expiresIn).getTime(),
-  //     },
-  //   },
-  //   {
-  //     name: 'heroku/users.page_sync.requested',
-  //     data: {
-  //       isFirstSync: true,
-  //       organisationId,
-  //       region,
-  //       syncStartedAt: Date.now(),
-  //       page: null,
-  //     },
-  //   },
-  // ]);
-  // return organisation;
+    await inngest.send([
+      {
+        name: 'heroku/token.refresh.requested',
+        data: {
+          organisationId,
+          expiresAt: addSeconds(new Date(), expiresIn).getTime(),
+        },
+      },
+      {
+        name: 'heroku/users.page_sync.requested',
+        data: {
+          isFirstSync: true,
+          organisationId,
+          region,
+          syncStartedAt: Date.now(),
+          page: null,
+        },
+      },
+    ]);
+    return organisation;
+  }
 };
--- apps/heroku/src/connectors/users.ts
+++ apps/heroku/src/connectors/users.ts
@@ -1,73 +1,55 @@
 import { HerokuError } from './commons/error';
+import { type HerokuUser } from './types';
 
-export type HerokuUser = {
-  role: string;
-  user: {
-    id: string;
-    name: string;
-    email: string;
-  };
-};
-
-export type HerokuPagination = {
+export type Pagination = {
   nextRange: string | null;
 };
 
-// type GetHerokuUsersResponseData = {HerokuUser[] };
-
-export const getHerokuUsers = async (
-  token: string,
-  teamId: string,
-  range?: string // Optional parameter to handle Range header
-): Promise<{ users: HerokuUser[]; pagination: HerokuPagination }> => {
+export const getUsers = async (token: string, teamId: string, range?: string) => {
   const headers: Record<string, string> = {
     Authorization: `Bearer ${token}`,
     Accept: 'application/vnd.heroku+json; version=3',
   };
 
-  // Add Range header if provided
   if (range) {
     headers.Range = range;
   }
+  const response = await fetch(`https://api.heroku.com/enterprise-accounts/${teamId}/members`, {
+    headers,
+  });
+  if (!response.ok) {
+    throw new HerokuError('Could not retrieve heroku users', { response });
+  }
+  const data = (await response.json()) as HerokuUser[];
 
+  if (response.status === 206) {
+    const pagination: Pagination = {
+      nextRange: response.headers.get('Next-Range'),
+    };
+    return { users: data, pagination };
+  }
+
+  const pagination: Pagination = {
+    nextRange: null,
+  };
+
+  return { users: data, pagination };
+};
+
+export const deleteUser = async (token: string, teamId: string, userId: string) => {
   const response = await fetch(
-    `https://api.heroku.com/teams/${teamId}/members`,
+    `https://api.heroku.com/enterprise-accounts/${teamId}/members/${userId}`,
     {
-      headers,
+      method: 'DELETE',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+        Accept: 'application/vnd.heroku+json; version=3',
+      },
     }
   );
 
   if (!response.ok) {
-    throw new HerokuError('Could not retrieve Heroku users', { response });
-  }
-
-  const responseData = (await response.json());
-
-  // Extract the Next-Range header from the response
-  const nextRangeHeader = response.headers.get('Next-Range');
-  
-  // Check if the status code is 206 Partial Content
-  const pagination: HerokuPagination = {
-    nextRange: response.status === 206 ? nextRangeHeader : null,
-  };
-
-
-
-  return { users: responseData, pagination };
-};
-export const deleteTeamMember = async (token: string, teamId: string, memberId: string) => {
-  const uri = `https://api.heroku.com/teams/${teamId}/members/${memberId}`;
-  
-  const response = await fetch(uri, {
-    method: 'DELETE',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
-      Accept: 'application/vnd.heroku+json; version=3',
-    },
-  });
-
-  if (!response.ok) {
-    throw new HerokuError(`Could not delete team member: ${memberId}`, { response });
+    throw new HerokuError(`Could not delete user with Id: ${userId}`, { response });
   }
 };
