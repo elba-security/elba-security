7f016e00d630d939d2db4c3c15e432677184b46a
mbliakharskyi <mbliakharskyi@gmail.com>
2024-03-19 11:01:29 +0200

    complete pagerduty

---
+++ apps/pager-duty/src/inngest/functions/users/delete-users.test.ts
@@ -0,0 +1,57 @@
+import { expect, test, describe, beforeEach, vi } from 'vitest';
+import { createInngestFunctionMock } from '@elba-security/test-utils';
+import { NonRetriableError } from 'inngest';
+import * as usersConnector from '@/connectors/users';
+import { Organisation } from '@/database/schema';
+import { encrypt } from '@/common/crypto';
+import { db } from '@/database/client';
+import { deleteSourceUsers } from './delete-users';
+
+const userId = '45a76301-f1dd-4a77-b12f-9d7d3fca3c90';
+const accessToken = 'test-access-token';
+const refreshToken = 'test-refresh-token';
+
+// Mock data for organisation and user
+const organisation = {
+  id: userId,
+  accessToken: await encrypt(accessToken),
+  refreshToken: await encrypt(refreshToken),
+  region: 'us',
+};
+
+// Setup function mock for Inngest
+const setup = createInngestFunctionMock(deleteSourceUsers, 'pagerduty/users.delete.requested');
+
+describe('deleteSourceUsers', () => {
+  beforeEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  test('should throw NonRetriableError when userid is not found', async () => {
+    // Mock database response to simulate no organisation found
+    vi.spyOn(usersConnector, 'deleteUsers').mockResolvedValueOnce();
+
+    const [result] = setup({ userId });
+
+    // Assert that the function throws a NonRetriableError
+    await expect(result).rejects.toBeInstanceOf(NonRetriableError);
+    await expect(result).rejects.toHaveProperty('message', `Could not retrieve ${userId}`);
+  });
+
+  test('should call deleteUsers with correct parameters', async () => {
+    // Mock database response to return organisation details
+    vi.spyOn(usersConnector, 'deleteUsers').mockResolvedValueOnce();
+    await db.insert(Organisation).values(organisation);
+
+    const [result] = setup({ userId });
+
+    // Assert the function resolves successfully
+    await expect(result).resolves.toStrictEqual(undefined);
+
+    expect(usersConnector.deleteUsers).toBeCalledTimes(1);
+    expect(usersConnector.deleteUsers).toBeCalledWith({
+      userId,
+      token: organisation.accessToken,
+    });
+  });
+});
