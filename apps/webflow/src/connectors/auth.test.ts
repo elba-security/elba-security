0c3089baf592bbececd0aa21f5cd62463c4955d6
Abdullah Awan <abdullahawan7676@gmail.com>
2024-03-21 04:18:27 +0500

    feat: auth flow implemented

--- apps/webflow/src/connectors/auth.test.ts
+++ apps/webflow/src/connectors/auth.test.ts
@@ -1,44 +1,39 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call -- test conveniency */
-/* eslint-disable @typescript-eslint/no-unsafe-return -- test conveniency */
-/**
- * DISCLAIMER:
- * The tests provided in this file are specifically designed for the `auth` connectors function.
- * Theses tests exists because the services & inngest functions using this connector mock it.
- * If you are using an SDK we suggest you to mock it not to implements calls using msw.
- * These file illustrate potential scenarios and methodologies relevant for SaaS integration.
- */
-
 import { http } from 'msw';
 import { describe, expect, test, beforeEach } from 'vitest';
 import { server } from '../../vitest/setup-msw-handlers';
-import { getToken } from './auth';
-import { MySaasError } from './commons/error';
+import { getAccessToken } from './auth';
+import { type WebflowError } from './commons/error';
 
-const validCode = '1234';
-const token = 'token-1234';
+const validAuthCode = 'valid-code';
+const accessToken = 'access-token';
 
-describe('auth connector', () => {
-  describe('getToken', () => {
-    // mock token API endpoint using msw
-    beforeEach(() => {
-      server.use(
-        http.post('https://mysaas.com/api/v1/token', async ({ request }) => {
-          // briefly implement API endpoint behaviour
-          const data = (await request.json()) as { code: string };
-          if (!data.code || data.code !== validCode) {
-            return new Response(undefined, { status: 401 });
-          }
-          return Response.json({ token });
-        })
-      );
-    });
+describe('getAccessToken', () => {
+  beforeEach(() => {
+    server.use(
+      http.post('https://api.webflow.com/oauth/access_token', ({ request }) => {
+        const url = new URL(request.url);
+        const code = url.searchParams.get('code');
+        if (code !== validAuthCode) {
+          return new Response(undefined, { status: 401 });
+        }
+        return new Response(JSON.stringify({ access_token: accessToken }), { status: 200 });
+      })
+    );
+  });
 
-    test('should return the token when the code is valid', async () => {
-      await expect(getToken(validCode)).resolves.toBe(token);
-    });
+  test('should not throw when authorization code is valid', async () => {
+    try {
+      await expect(getAccessToken(validAuthCode)).resolves.toStrictEqual(accessToken);
+    } catch (error) {
+      expect(error).toBeNull();
+    }
+  });
 
-    test('should throw when the code is invalid', async () => {
-      await expect(getToken('wrong-code')).rejects.toBeInstanceOf(MySaasError);
-    });
+  test('should throw an error when authorization code is invalid', async () => {
+    try {
+      await getAccessToken('invalid-auth-code');
+    } catch (error) {
+      expect((error as WebflowError).message).toBe('Failed to fetch');
+    }
   });
 });
