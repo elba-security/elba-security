003d0bfa70af464d1ad205f9763590e3e5142a37
Ahmed Hashir <124239470+AhmedHashir10@users.noreply.github.com>
2024-03-25 12:30:51 +0500

    feat:Authentication flow implemented

--- apps/livestorm/src/database/schema.ts
+++ apps/livestorm/src/database/schema.ts
@@ -1,12 +1,8 @@
-import { uuid, text, timestamp, pgTable, boolean } from 'drizzle-orm/pg-core';
-import { type InferSelectModel } from 'drizzle-orm';
+import { uuid, text, timestamp, pgTable } from 'drizzle-orm/pg-core';
 
 export const Organisation = pgTable('organisation', {
-  id: uuid('id').primaryKey(),
-  region: text('region').notNull(),
-  createdAt: timestamp('created_at').defaultNow().notNull(),
-  // the following properties are examples, it can removed / replaced to fit your use-case
-  token: text('token').notNull(),
+id: uuid('id').primaryKey(),
+region: text('region').notNull(),
+token: text('token').notNull(),
+createdAt: timestamp('created_at').defaultNow().notNull(),
 });
-
-export type SelectOrganisation = InferSelectModel<typeof Organisation>;
