b59128f10c1bda71157647660d8f6d3e7a36d887
Ahmed Hashir <124239470+AhmedHashir10@users.noreply.github.com>
2024-03-25 16:48:59 +0500

    feat:delete users webhook flow implemented

--- apps/livestorm/src/inngest/functions/index.ts
+++ apps/livestorm/src/inngest/functions/index.ts
@@ -1,3 +1,6 @@
+import { removeOrganisation } from './organisation/remove-organisation';
+import { deleteLivestormUser } from './users/delete.user';
+import { scheduleUsersSyncs } from './users/schedule-users-syncs';
 import { syncUsersPage } from './users/sync-users-page';
 
-export const inngestFunctions = [syncUsersPage];
+export const inngestFunctions = [syncUsersPage,scheduleUsersSyncs,deleteLivestormUser,removeOrganisation];
