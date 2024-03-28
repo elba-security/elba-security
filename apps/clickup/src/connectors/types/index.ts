3787f28742e6cea5bd092a6da67a8e4d2f63c56f
Ahmed Hashir <124239470+AhmedHashir10@users.noreply.github.com>
2024-03-29 02:34:27 +0500

    feat:get team id flow implemeted

---
+++ apps/clickup/src/connectors/types/index.ts
@@ -0,0 +1,25 @@
+export type GetTokenResponseData = { access_token: string; token_type: string; scope: string };
+
+
+export type ClickUpTeam = {
+ id: string;
+ name: string;
+};
+
+
+export type GetTeamResponseData = {
+ teams: ClickUpTeam[];
+};
+
+
+export type ClickUpUser = {
+ id: string;
+ email: string;
+ username: string;
+ role:string;
+};
+
+
+export type GetUsersResponseData = {
+ users: ClickUpUser[];
+};
