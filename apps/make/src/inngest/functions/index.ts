ca27aeda686b6ac0330d264a99fba207c0408111
haisim-y <haisimyasin@outlook.com>
2024-04-03 13:59:39 +0500

    Implement remove-organisation functionality. Add remove-organisation and rate-limit-middleware tests

--- apps/make/src/inngest/functions/index.ts
+++ apps/make/src/inngest/functions/index.ts
@@ -1,3 +1,5 @@
+import { scheduleUsersSyncs } from './users/schedule-users-syncs';
 import { syncUsersPage } from './users/sync-users-page';
+import { removeOrganisation } from './organisations/remove-organisation';
 
-export const inngestFunctions = [syncUsersPage];
+export const inngestFunctions = [syncUsersPage, scheduleUsersSyncs, removeOrganisation];
