626927e850753911a4806c4668fec1131b72eb90
Athithan <athithan@elba.security>
2024-04-22 13:28:18 +0200

    fix: ignore docusign from doppler pr

--- apps/docusign/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-/**
- * This file is required by nextjs and has no purpose for now in the integration.
- * It should not be edited or removed.
- */
-export default function Home() {
-  return <main>Elba x Docusign</main>;
-}
--- apps/docusign/src/inngest/functions/index.ts
+++
@@ -1,5 +0,0 @@
-import { synchronizeUsers } from './users/synchronize-users';
-import { scheduleUsersSynchronize } from './users/schedule-users-synchronize';
-import { deleteSourceUsers } from './users/delete-users';
-
-export const inngestFunctions = [synchronizeUsers, scheduleUsersSynchronize, deleteSourceUsers];
