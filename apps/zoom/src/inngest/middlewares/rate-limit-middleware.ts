eba5b2f603952952fde89a250fde8b52048790be
mbliakharskyi <mbliakharskyi@gmail.com>
2024-04-24 07:09:39 +0300

    feat: add delete endpoint and test listing users

---
+++ apps/zoom/src/inngest/middlewares/rate-limit-middleware.ts
@@ -0,0 +1,47 @@
+import { InngestMiddleware, RetryAfterError } from 'inngest';
+import { ZoomError } from '@/connectors/commons/error';
+
+export const rateLimitMiddleware = new InngestMiddleware({
+  name: 'rate-limit',
+  init: () => {
+    return {
+      onFunctionRun: ({ fn }) => {
+        return {
+          transformOutput: (ctx) => {
+            const {
+              result: { error, ...result },
+              ...context
+            } = ctx;
+
+            if (!(error instanceof ZoomError)) {
+              // Adjust based on actual error handling
+              return;
+            }
+
+            if (error.response?.status === 429) {
+              let retryAfter = 60;
+              const retryAfterHeader = error.response.headers.get('retry-after');
+              if (retryAfterHeader) {
+                retryAfter = parseInt(retryAfterHeader, 10);
+              }
+
+              return {
+                ...context,
+                result: {
+                  ...result,
+                  error: new RetryAfterError(
+                    `API rate limit reached by '${fn.name}', retry after ${retryAfter} seconds.`,
+                    retryAfter.toString(),
+                    {
+                      cause: error,
+                    }
+                  ),
+                },
+              };
+            }
+          },
+        };
+      },
+    };
+  },
+});
