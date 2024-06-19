e52683ef75440ea39b2fbc14a0887616e8e9cb91
Maksym <148101061+mbliakharskyi@users.noreply.github.com>
2024-06-19 17:48:04 +0300

    feat(calendly): refactor codebase (#328)
    ## Description
    
    Please include a summary of the changes and issues addressed in this PR.
    Also, explain the motivation and context of each update. List any
    dependencies or prerequisites needed for implementing this change.
    
    ## Additional Notes
    
    Add any other context or screenshots about the feature request here.
    
    ## Checklist:
    
    Before you create this PR, confirm all the requirements listed below by
    checking the checkboxes like this (`[x]`).
    
    - [ ] I have performed a self-review of my own code.
    - [ ] I have commented my code, particularly in hard-to-understand
    areas.
    - [ ] I have added tests to cover the new feature or fixes.
    
    ---------
    
    Co-authored-by: DESKTOP-8Q1MBK4\Administrator <Maksym>

---
+++ apps/calendly/src/connectors/calendly/auth.ts
@@ -0,0 +1,94 @@
+import { z } from 'zod';
+import { logger } from '@elba-security/logger';
+import { env } from '@/common/env';
+import { CalendlyError } from '../common/error';
+
+const tokenResponseSchema = z.object({
+  access_token: z.string(),
+  refresh_token: z.string(),
+  organization: z.string(),
+  expires_in: z.number(),
+});
+
+export const getToken = async (code: string) => {
+  const encodedKey = Buffer.from(
+    `${env.CALENDLY_CLIENT_ID}:${env.CALENDLY_CLIENT_SECRET}`
+  ).toString('base64');
+
+  const response = await fetch(`${env.CALENDLY_APP_INSTALL_URL}/token`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: `${encodedKey}`,
+    },
+    body: new URLSearchParams({
+      client_id: env.CALENDLY_CLIENT_ID,
+      client_secret: env.CALENDLY_CLIENT_SECRET,
+      grant_type: 'authorization_code',
+      redirect_uri: env.CALENDLY_REDIRECT_URI,
+      code,
+    }),
+  });
+
+  if (!response.ok) {
+    throw new CalendlyError('Could not retrieve token', { response });
+  }
+
+  const data: unknown = await response.json();
+
+  const result = tokenResponseSchema.safeParse(data);
+
+  if (!result.success) {
+    logger.error('Invalid Calendly token response', { data });
+    throw new CalendlyError('Invalid Calendly token response');
+  }
+
+  return {
+    accessToken: result.data.access_token,
+    refreshToken: result.data.refresh_token,
+    expiresIn: result.data.expires_in,
+    organizationUri: result.data.organization,
+  };
+};
+
+export const getRefreshToken = async (refreshToken: string) => {
+  const encodedKey = Buffer.from(
+    `${env.CALENDLY_CLIENT_ID}:${env.CALENDLY_CLIENT_SECRET}`
+  ).toString('base64');
+
+  const response = await fetch(`${env.CALENDLY_APP_INSTALL_URL}/token`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: `${encodedKey}`,
+    },
+    body: new URLSearchParams({
+      client_id: env.CALENDLY_CLIENT_ID,
+      client_secret: env.CALENDLY_CLIENT_SECRET,
+      grant_type: 'refresh_token',
+      refresh_token: refreshToken,
+    }),
+  });
+
+  if (!response.ok) {
+    throw new CalendlyError('Could not retrieve token', { response });
+  }
+
+  const data: unknown = await response.json();
+
+  const result = tokenResponseSchema.safeParse(data);
+
+  if (!result.success) {
+    logger.error('Invalid Calendly refresh token response', {
+      data,
+      result: JSON.stringify(result, null, 2),
+    });
+    throw new Error('Invalid Calendly token response');
+  }
+
+  return {
+    accessToken: result.data.access_token,
+    refreshToken: result.data.refresh_token,
+    expiresIn: result.data.expires_in,
+  };
+};
