b5648cac95def02995e7287744c3251fa4d25cd0
Abdullah Awan <abdullahawan7676@gmail.com>
2024-06-21 16:16:46 +0500

    refactor: global setup configured

--- apps/make/drizzle/0000_shocking_lethal_legion.sql
+++ apps/make/drizzle/0000_shocking_lethal_legion.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS "organisation" (
+CREATE TABLE IF NOT EXISTS "organisations" (
 	"id" uuid PRIMARY KEY NOT NULL,
 	"token" text NOT NULL,
 	"zone_domain" text NOT NULL,
--- apps/make/src/database/schema.ts
+++ apps/make/src/database/schema.ts
@@ -1,7 +1,7 @@
 import { uuid, text, timestamp, pgTable } from 'drizzle-orm/pg-core';
 import { type InferSelectModel } from 'drizzle-orm';
 
-export const Organisation = pgTable('organisation', {
+export const Organisation = pgTable('organisations', {
   id: uuid('id').primaryKey(),
   region: text('region').notNull(),
   createdAt: timestamp('created_at').defaultNow().notNull(),
