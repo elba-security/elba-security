4aa64bf4e7d1b6cfb1738670e89da5ba1c45b12d
Maksym <148101061+mbliakharskyi@users.noreply.github.com>
2024-06-24 16:30:37 +0300

    feat(salesforce); OAuth flow (#337)
    Please include a summary of the changes and issues addressed in this PR.
    Also, explain the motivation and context of each update. List any
    dependencies or prerequisites needed for implementing this change.
    
    Add any other context or screenshots about the feature request here.
    
    Before you create this PR, confirm all the requirements listed below by
    checking the checkboxes like this (`[x]`).
    
    - [x] I have performed a self-review of my own code.
    - [x] I have commented my code, particularly in hard-to-understand
    areas.
    - [x] I have added tests to cover the new feature or fixes.
    
    ---------
    
    Co-authored-by: DESKTOP-8Q1MBK4\Administrator <Maksym>

---
+++ apps/salesforce/src/app/auth/service.ts
@@ -0,0 +1,69 @@
+import { db } from '@/database/client';
+import { organisationsTable } from '@/database/schema';
+import { getToken , getExpiresIn } from '@/connectors/salesforce/auth';
+import { inngest } from '@/inngest/client';
+import { encrypt } from '@/common/crypto';
+
+type SetupOrganisationParams = {
+  organisationId: string;
+  code: string;
+  region: string;
+};
+
+export const setupOrganisation = async ({
+  organisationId,
+  code,
+  region,
+}: SetupOrganisationParams) => {
+  const { accessToken, refreshToken, instanceUrl } = await getToken(code);
+
+  const tokenType = 'access_token';
+  const { expiresAt } = await getExpiresIn({ token: accessToken, tokenType });
+
+  const encryptedAccessToken = await encrypt(accessToken);
+  const encryptedRefreshToken = await encrypt(refreshToken);
+
+  await db
+    .insert(organisationsTable)
+    .values({
+      id: organisationId,
+      accessToken: encryptedAccessToken,
+      refreshToken: encryptedRefreshToken,
+      instanceUrl,
+      region,
+    })
+    .onConflictDoUpdate({
+      target: organisationsTable.id,
+      set: {
+        accessToken: encryptedAccessToken,
+        refreshToken: encryptedRefreshToken,
+        instanceUrl,
+        region,
+      },
+    });
+
+  await inngest.send([
+    {
+      name: 'salesforce/users.sync.requested',
+      data: {
+        organisationId,
+        isFirstSync: true,
+        syncStartedAt: Date.now(),
+        page: 0,
+      },
+    },
+    {
+      name: 'salesforce/app.installed',
+      data: {
+        organisationId,
+      },
+    },
+    {
+      name: 'salesforce/token.refresh.requested',
+      data: {
+        organisationId,
+        expiresAt: expiresAt * 1000,
+      },
+    },
+  ]);
+};
