3ff19305c04f19ed145a959be333ccf8417ee7bf
Maksym <148101061+mbliakharskyi@users.noreply.github.com>
2024-07-05 17:44:17 +0300

    feat(zendesk): OAuth flow and Sync users (#350)
    ## Description
    
    1. Refactoring codebase such as OAuth, Sync users
    2. The toke doesn't expire
    3. It support user suspend endpoint
    
    ## Additional Notes
    
    Add any other context or screenshots about the feature request here.
    
    ## Checklist:
    
    Before you create this PR, confirm all the requirements listed below by
    checking the checkboxes like this (`[x]`).
    
    - [x] I have performed a self-review of my own code.
    - [x] I have commented my code, particularly in hard-to-understand
    areas.
    - [x] I have added tests to cover the new feature or fixes.

---
+++ apps/zendesk/src/inngest/client.ts
@@ -0,0 +1,36 @@
+import { EventSchemas, Inngest } from 'inngest';
+import { sentryMiddleware } from '@elba-security/inngest';
+import { logger } from '@elba-security/logger';
+import { rateLimitMiddleware } from './middlewares/rate-limit-middleware';
+
+export const inngest = new Inngest({
+  id: 'zendesk',
+  schemas: new EventSchemas().fromRecord<{
+    'zendesk/users.sync.requested': {
+      data: {
+        organisationId: string;
+        isFirstSync: boolean;
+        syncStartedAt: number;
+        page: string | null;
+      };
+    };
+    'zendesk/app.installed': {
+      data: {
+        organisationId: string;
+      };
+    };
+    'zendesk/app.uninstalled': {
+      data: {
+        organisationId: string;
+      };
+    };
+    'zendesk/users.delete.requested': {
+      data: {
+        organisationId: string;
+        userId: string;
+      };
+    };
+  }>(),
+  middleware: [rateLimitMiddleware, sentryMiddleware],
+  logger,
+});
