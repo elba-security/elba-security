e2e73f950593dc878e4b94d137aa3c82998fbdcd
athithan <athithan@elba.security>
2024-07-29 13:38:59 +0200

    feat(dbx-data-protection): data protection included

--- apps/dropbox/src/app/api/webhooks/elba/data-protection/delete-object-permissions/service.ts
+++ apps/dropbox/src/app/api/webhooks/elba/data-protection/delete-object-permissions/service.ts
@@ -1,5 +1,25 @@
-import { DeleteObjectPermissions } from '@/connectors/types';
 import { inngest } from '@/inngest/client';
+import { z } from 'zod';
+
+const deleteObjectPermissionsSchema = z.object({
+  id: z.string(),
+  organisationId: z.string(),
+  metadata: z.object({
+    ownerId: z.string(),
+    type: z.union([z.literal('file'), z.literal('folder')]),
+    isPersonal: z.boolean(),
+  }),
+  permissions: z.array(
+    z.object({
+      id: z.string(),
+      metadata: z.object({
+        sharedLinks: z.array(z.string()).optional(),
+      }),
+    })
+  ),
+});
+
+export type DeleteObjectPermissions = z.infer<typeof deleteObjectPermissionsSchema>;
 
 export const deleteObjectPermissions = async (data: DeleteObjectPermissions) => {
   await inngest.send(
