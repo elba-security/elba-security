85084484a97c2a630d28f2ca66ecbc6479e26c23
athithan <athithan@elba.security>
2024-08-13 11:21:59 +0200

    chore: remove camelcase package

--- apps/dropbox-edge/src/connectors/elba/third-party-apps.ts
+++ apps/dropbox-edge/src/connectors/elba/third-party-apps.ts
@@ -1,35 +1,17 @@
 import type { ThirdPartyAppsObject } from '@elba-security/sdk';
-import { z } from 'zod';
-import camelcaseKeys from 'camelcase-keys';
 import type { LinkedApps } from '../dropbox/apps';
-import { linkedAppsSchema } from '../dropbox/apps';
-
-type CamelCaseLinkedApps = {
-  teamMemberId: string;
-  linkedApiApps: {
-    appId: string;
-    appName: string;
-    linked?: string;
-    publisher?: string;
-    publisherUrl?: string;
-  }[];
-}[];
-
-// Dropbox linked apps  properties are in snake_case, therefore we need to convert them to camelCase
-// Reassigning the transformed schema to a new variable to avoid mutating the original schema
-const linkedAppsSchemaCamelKeys = z.array(linkedAppsSchema).transform((data) => {
-  return camelcaseKeys(data, { deep: true });
-});
-
-export type LinkedAppsM = z.infer<typeof linkedAppsSchemaCamelKeys>;
 
 export const formatThirdPartyObjects = (apps: LinkedApps[]) => {
-  const memberLinkedApps: CamelCaseLinkedApps = linkedAppsSchemaCamelKeys.parse(apps);
-
   const thirdPartyApps = new Map<string, ThirdPartyAppsObject>();
 
-  for (const { linkedApiApps, teamMemberId } of memberLinkedApps) {
-    for (const { appId, appName, linked, publisher, publisherUrl } of linkedApiApps) {
+  for (const { linked_api_apps: linkedApiApps, team_member_id: teamMemberId } of apps) {
+    for (const {
+      app_id: appId,
+      app_name: appName,
+      linked,
+      publisher,
+      publisher_url: publisherUrl,
+    } of linkedApiApps) {
       const thirdPartyApp = thirdPartyApps.get(appId);
 
       if (thirdPartyApp) {
