e0a50b512c46ad083677b0d2f15ceaa5e4992bb3
Maksym <148101061+mbliakharskyi@users.noreply.github.com>
2024-11-12 16:41:29 +0200

    feat(salesloft): OAuth, sync users and handle the api rate limit  (#445)
    ## Description
    
    Salesloft Integration
    
    1. Implemented OAuth and Sync users.
    2. It's possible to deactivate a user but didn't implemented yet because
    couldn't get the owner's id.
    3. Tested all the features.
    
    ## Additional Notes
    
    Add any other context or screenshots about the feature request here.
    
    ## Checklist:
    
    Before you create this PR, confirm all the requirements listed below by
    checking the checkboxes like this (`[x]`).
    
    - [ ] I have performed a self-review of my own code.
    - [ ] I have commented my code, particularly in hard-to-understand
    areas.
    - [ ] I have added tests to cover the new feature or fixes.
    
    ---------
    
    Co-authored-by: Athithan <104758366+athielba@users.noreply.github.com>

---
+++ apps/salesloft/src/connectors/salesloft/users.test.ts
@@ -0,0 +1,67 @@
+import { http } from 'msw';
+import { describe, expect, test, beforeEach } from 'vitest';
+import { server } from '@elba-security/test-utils';
+import { env } from '@/common/env';
+import { SalesloftError } from '../common/error';
+import type { SalesloftUser } from './users';
+import { getUsers } from './users';
+
+const validToken = 'token-1234';
+const endPageToken = 3;
+const nextPageToken = 2;
+
+const validUsers: SalesloftUser[] = Array.from({ length: 5 }, (_, i) => ({
+  id: i,
+  name: `name-${i}`,
+  email: `user-${i}@foo.bar`,
+}));
+
+const invalidUsers = [];
+
+describe('users connector', () => {
+  describe('getUsers', () => {
+    beforeEach(() => {
+      server.use(
+        http.get(`${env.SALESLOFT_API_BASE_URL}/v2/users`, ({ request }) => {
+          if (request.headers.get('Authorization') !== `Bearer ${validToken}`) {
+            return new Response(undefined, { status: 401 });
+          }
+
+          const url = new URL(request.url);
+          const pageToken = url.searchParams.get('page') ?? '0';
+          const responseData = {
+            data: validUsers,
+            metadata: {
+              paging: { next_page: pageToken === String(endPageToken) ? null : nextPageToken },
+            },
+          };
+          return Response.json(responseData);
+        })
+      );
+    });
+
+    test('should return users and nextPage when the token is valid and their is another page', async () => {
+      await expect(
+        getUsers({ accessToken: validToken, page: String(nextPageToken) })
+      ).resolves.toStrictEqual({
+        validUsers,
+        invalidUsers,
+        nextPage: String(nextPageToken),
+      });
+    });
+
+    test('should return users and no nextPage when the token is valid and their is no other page', async () => {
+      await expect(
+        getUsers({ accessToken: validToken, page: String(endPageToken) })
+      ).resolves.toStrictEqual({
+        validUsers,
+        invalidUsers,
+        nextPage: null,
+      });
+    });
+
+    test('should throws when the token is invalid', async () => {
+      await expect(getUsers({ accessToken: 'foo-bar' })).rejects.toBeInstanceOf(SalesloftError);
+    });
+  });
+});
