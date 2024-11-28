6d6dd0605415b1e42f06b5d66ab048ad85615acc
mbliakharskyi <mbliakharskyi@gmail.com>
2024-11-28 03:50:33 +0200

    feat: add nango feature

--- apps/intercom/src/connectors/intercom/users.test.ts
+++ apps/intercom/src/connectors/intercom/users.test.ts
@@ -1,7 +1,7 @@
 import { http } from 'msw';
 import { describe, expect, test, beforeEach } from 'vitest';
 import { server } from '@elba-security/test-utils';
-import { env } from '@/common/env';
+import { env } from '@/common/env/server';
 import { IntercomError } from '../common/error';
 import type { IntercomUser } from './users';
 import { getUsers } from './users';
