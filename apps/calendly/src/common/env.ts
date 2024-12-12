5f89d62aa2d97842040729827db135eed0fe3884
mbliakharskyi <mbliakharskyi@gmail.com>
2024-12-12 12:12:58 +0200

    feat: add nango refactoring

--- apps/calendly/src/common/env.ts
+++ apps/calendly/src/common/env.ts
@@ -6,18 +6,13 @@ export const env = z
   .object({
     ELBA_API_KEY: z.string().min(1),
     ELBA_API_BASE_URL: z.string().url(),
-    ELBA_REDIRECT_URL: z.string().url(),
     ELBA_SOURCE_ID: z.string().uuid(),
     ELBA_WEBHOOK_SECRET: z.string().min(1),
-    DATABASE_URL: z.string().min(1),
-    DATABASE_PROXY_PORT: zEnvInt().optional(),
     CALENDLY_API_BASE_URL: z.string().url().default('https://api.calendly.com'),
     CALENDLY_DELETE_USER_CONCURRENCY: zEnvInt().default(5),
     CALENDLY_USERS_SYNC_BATCH_SIZE: zEnvInt().default(20),
     CALENDLY_USERS_SYNC_CRON: z.string().default('0 0 * * *'),
-    NEXT_PUBLIC_NANGO_INTEGRATION_ID: z.string().min(1),
-    NEXT_PUBLIC_NANGO_PUBLIC_KEY: z.string().min(1),
     NANGO_SECRET_KEY: z.string().min(1),
-    VERCEL_ENV: z.string().min(1).optional(),
+    NANGO_INTEGRATION_ID: z.string().min(1),
   })
   .parse(process.env);
