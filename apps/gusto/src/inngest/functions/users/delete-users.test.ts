2aa8c37687dddced815e8191bcef6b9400a4de35
mbliakharskyi <mbliakharskyi@gmail.com>
2024-12-16 23:11:45 +0200

    feat: nango refactor

--- apps/gusto/src/inngest/functions/users/delete-users.test.ts
+++ apps/gusto/src/inngest/functions/users/delete-users.test.ts
@@ -1,22 +1,14 @@
 import { expect, test, describe, beforeEach, vi } from 'vitest';
 import { createInngestFunctionMock } from '@elba-security/test-utils';
 import * as usersConnector from '@/connectors/gusto/users';
-import { organisationsTable } from '@/database/schema';
-import { encrypt } from '@/common/crypto';
-import { db } from '@/database/client';
+import * as nangoAPI from '@/common/nango';
 import { deleteUser } from './delete-users';
 
 const userId = 'user-id';
 const accessToken = 'test-access-token';
-const refreshToken = 'test-refresh-token';
-const authUserEmail = 'test-auth-user-email';
 const organisation = {
   id: '00000000-0000-0000-0000-000000000001',
-  accessToken: await encrypt(accessToken),
-  refreshToken: await encrypt(refreshToken),
   region: 'us',
-  companyId: 'https://api.gusto.com/users/AAAAAAAAAAAAAAAA',
-  authUserEmail,
 };
 
 const setup = createInngestFunctionMock(deleteUser, 'gusto/users.delete.requested');
@@ -24,11 +16,22 @@ const setup = createInngestFunctionMock(deleteUser, 'gusto/users.delete.requeste
 describe('deleteUser', () => {
   beforeEach(() => {
     vi.restoreAllMocks();
+
+    const mockNangoAPIClient = {
+      getConnection: vi.fn().mockResolvedValue({
+        credentials: {
+          access_token: accessToken,
+        },
+      }),
+    };
+
+    vi.spyOn(nangoAPI, 'nangoAPIClient', 'get').mockReturnValue(
+      mockNangoAPIClient as unknown as typeof nangoAPI.nangoAPIClient
+    );
   });
 
-  test('should delete users', async () => {
+  test('should delete user', async () => {
     vi.spyOn(usersConnector, 'deleteUser').mockResolvedValueOnce();
-    await db.insert(organisationsTable).values(organisation);
 
     const [result] = setup({ userId, organisationId: organisation.id });
 
--- apps/gusto/src/inngest/functions/users/schedule-users-sync.ts
+++ apps/gusto/src/inngest/functions/users/schedule-users-sync.ts
@@ -1,7 +1,9 @@
+import { elbaRegions } from '@elba-security/sdk';
+import { logger } from '@elba-security/logger';
+import { NonRetriableError } from 'inngest';
 import { env } from '@/common/env';
-import { db } from '@/database/client';
-import { organisationsTable } from '@/database/schema';
-import { inngest } from '@/inngest/client';
+import { createElbaGlobalClient } from '@/connectors/elba/client';
+import { inngest } from '../../client';
 
 export const scheduleUsersSync = inngest.createFunction(
   {
@@ -10,24 +12,57 @@ export const scheduleUsersSync = inngest.createFunction(
   },
   { cron: env.GUSTO_USERS_SYNC_CRON },
   async ({ step }) => {
-    const organisations = await db
-      .select({
-        id: organisationsTable.id,
-      })
-      .from(organisationsTable);
+    const regionOrganisations = await Promise.all(
+      elbaRegions.map((region) =>
+        step.run(`get-organisations-${region}`, async () => {
+          const elba = createElbaGlobalClient(region);
+          const result = await elba.organisations.list();
+          return result.organisations.map(({ id: organisationId, nangoConnectionId }) => ({
+            organisationId,
+            nangoConnectionId,
+            region,
+          }));
+        })
+      )
+    );
 
-    if (organisations.length > 0) {
+    const invalidOrganisations: { organisationId: string; region: string }[] = [];
+    const organisations: {
+      organisationId: string;
+      region: string;
+      nangoConnectionId: string;
+    }[] = [];
+    for (const { organisationId, region, nangoConnectionId } of regionOrganisations.flat()) {
+      if (!nangoConnectionId) {
+        invalidOrganisations.push({ organisationId, region });
+      } else {
+        organisations.push({ organisationId, region, nangoConnectionId });
+      }
+    }
+
+    if (organisations.length) {
       await step.sendEvent(
         'sync-users',
-        organisations.map(({ id }) => ({
+        organisations.map(({ organisationId, nangoConnectionId, region }) => ({
           name: 'gusto/users.sync.requested',
           data: {
             isFirstSync: false,
-            organisationId: id,
+            organisationId,
             syncStartedAt: Date.now(),
             page: 1,
+            nangoConnectionId,
+            region,
           },
         }))
+      );
+    }
+
+    if (invalidOrganisations.length) {
+      logger.error('Failed to schedule users sync due to missing nango connection ID', {
+        invalidOrganisations,
+      });
+      throw new NonRetriableError(
+        'Failed to schedule users sync due to missing nango connection ID'
       );
     }
 
