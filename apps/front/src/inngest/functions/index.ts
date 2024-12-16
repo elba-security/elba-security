1b0c52de60c88370f1b4d9bc25b6e184fdfd42ef
mbliakharskyi <mbliakharskyi@gmail.com>
2024-12-16 06:07:57 +0200

    feat: add nango

--- apps/front/src/inngest/functions/index.ts
+++ apps/front/src/inngest/functions/index.ts
@@ -1,6 +1,5 @@
 import { removeOrganisation } from './organisations/remove-organisation';
 import { syncUsers } from './users/sync-users';
 import { scheduleUsersSync } from './users/schedule-users-sync';
-import { refreshToken } from './token/refresh-token';
 
-export const inngestFunctions = [refreshToken, removeOrganisation, scheduleUsersSync, syncUsers];
+export const inngestFunctions = [removeOrganisation, scheduleUsersSync, syncUsers];
