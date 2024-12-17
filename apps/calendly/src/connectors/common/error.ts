2360a516c33c17590cca3baef8b3befb52a1c20e
Athithan <104758366+athielba@users.noreply.github.com>
2024-12-17 15:01:43 +0100

    feat(calendly): nango implementation  (#495)
    ## Description
    
    Nango implementations
    
    ## Additional Notes
    
    Add any other context or screenshots about the feature request here.
    
    ## Checklist:
    
    Before you create this PR, confirm all the requirements listed below by
    checking the checkboxes like this (`[x]`).
    
    - [ ] I have performed a self-review of my own code.
    - [ ] I have commented my code, particularly in hard-to-understand
    areas.
    - [ ] I have added tests to cover the new feature or fixes.
    
    ## Description
    
    Please include a summary of the changes and issues addressed in this PR.
    Also, explain the motivation and context of each update. List any
    dependencies or prerequisites needed for implementing this change.
    
    ## Additional Notes
    
    Add any other context or screenshots about the feature request here.
    
    ## Checklist:
    
    Before you create this PR, confirm all the requirements listed below by
    checking the checkboxes like this (`[x]`).
    
    - [ ] I have performed a self-review of my own code.
    - [ ] I have commented my code, particularly in hard-to-understand
    areas.
    - [ ] I have added tests to cover the new feature or fixes.
    
    ---------
    
    Co-authored-by: Maksym <148101061+mbliakharskyi@users.noreply.github.com>
    Co-authored-by: Guillaume PROQUIN <guillaume.proquin@epitech.eu>

--- apps/calendly/src/connectors/common/error.ts
+++ apps/calendly/src/connectors/common/error.ts
@@ -1,3 +1,6 @@
+import { type MapConnectionErrorFn } from '@elba-security/inngest';
+import { NangoConnectionError } from '@elba-security/nango';
+
 type CalendlyErrorOptions = { response?: Response };
 
 export class CalendlyError extends Error {
@@ -6,5 +9,22 @@ export class CalendlyError extends Error {
   constructor(message: string, { response }: CalendlyErrorOptions = {}) {
     super(message);
     this.response = response;
+    this.name = 'CalendlyError';
   }
 }
+
+export class CalendlyUnsupportedPlanError extends CalendlyError {}
+
+export const mapElbaConnectionError: MapConnectionErrorFn = (error) => {
+  if (error instanceof NangoConnectionError && error.response.status === 404) {
+    return 'unauthorized';
+  }
+  if (error instanceof CalendlyError && error.response?.status !== 401) {
+    return 'unauthorized';
+  }
+  if (error instanceof CalendlyUnsupportedPlanError) {
+    return 'unsupported_plan';
+  }
+
+  return null;
+};
