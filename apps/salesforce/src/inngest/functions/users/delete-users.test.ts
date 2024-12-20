0bf9233c3bd8af4e0b541414f3e6be583a333cd1
mbliakharskyi <mbliakharskyi@gmail.com>
2024-12-20 10:12:41 +0200

    feat: nango refactor

--- apps/salesforce/src/inngest/functions/users/delete-users.test.ts
+++ apps/salesforce/src/inngest/functions/users/delete-users.test.ts
@@ -1,24 +1,15 @@
 import { expect, test, describe, beforeEach, vi } from 'vitest';
 import { createInngestFunctionMock } from '@elba-security/test-utils';
 import * as usersConnector from '@/connectors/salesforce/users';
-import { organisationsTable } from '@/database/schema';
-import { encrypt } from '@/common/crypto';
-import { db } from '@/database/client';
+import * as nangoAPI from '@/common/nango';
 import { deleteUser } from './delete-users';
 
 const userId = '00000000-0000-0000-0000-000000000001';
 const accessToken = 'test-access-token';
-const refreshToken = 'test-refresh-token';
-const instanceUrl = 'test-some url';
-
-const organisation = {
-  id: userId,
-  authUserId: '00000001',
-  accessToken: await encrypt(accessToken),
-  refreshToken: await encrypt(refreshToken),
-  instanceUrl,
-  region: 'us',
-};
+const organisationId = '00000000-0000-0000-0000-000000000001';
+const nangoConnectionId = 'nango-connection-id';
+const region = 'us';
+const instanceUrl = 'https://test-some-url.com';
 
 const setup = createInngestFunctionMock(deleteUser, 'salesforce/users.delete.requested');
 
@@ -29,9 +20,14 @@ describe('deleteUser', () => {
 
   test('should delete user', async () => {
     vi.spyOn(usersConnector, 'deleteUser').mockResolvedValueOnce();
-    await db.insert(organisationsTable).values(organisation);
+    // @ts-expect-error -- this is a mock
+    vi.spyOn(nangoAPI, 'nangoAPIClient', 'get').mockReturnValue({
+      getConnection: vi.fn().mockResolvedValue({
+        credentials: { access_token: 'access-token' },
+      }),
+    });
 
-    const [result] = setup({ userId, organisationId: organisation.id });
+    const [result] = setup({ organisationId, region, nangoConnectionId, userId });
 
     await expect(result).resolves.toStrictEqual(undefined);
 
--- apps/salesforce/src/inngest/functions/users/delete-users.ts
+++ apps/salesforce/src/inngest/functions/users/delete-users.ts
@@ -1,11 +1,9 @@
-import { eq } from 'drizzle-orm';
 import { NonRetriableError } from 'inngest';
-import { db } from '@/database/client';
-import { organisationsTable } from '@/database/schema';
 import { inngest } from '@/inngest/client';
 import { deleteUser as deleteSalesforceUser } from '@/connectors/salesforce/users';
-import { decrypt } from '@/common/crypto';
 import { env } from '@/common/env';
+import { nangoAPIClient } from '@/common/nango';
+import { credentialsRawSchema } from './sync-users';
 
 export const deleteUser = inngest.createFunction(
   {
@@ -24,26 +22,28 @@ export const deleteUser = inngest.createFunction(
   },
   { event: 'salesforce/users.delete.requested' },
   async ({ event }) => {
-    const { userId, organisationId } = event.data;
+    const { nangoConnectionId, userId } = event.data;
 
-    const [organisation] = await db
-      .select({
-        accessToken: organisationsTable.accessToken,
-        instanceUrl: organisationsTable.instanceUrl,
-      })
-      .from(organisationsTable)
-      .where(eq(organisationsTable.id, organisationId));
+    const { credentials } = await nangoAPIClient.getConnection(nangoConnectionId);
 
-    if (!organisation) {
-      throw new NonRetriableError(`Could not retrieve ${userId}`);
+    if (!('access_token' in credentials) || typeof credentials.access_token !== 'string') {
+      throw new NonRetriableError('Could not retrieve Nango credentials');
     }
-    const accessToken = await decrypt(organisation.accessToken);
-    const instanceUrl = organisation.instanceUrl;
+
+    if (!('instance_url' in credentials.raw)) {
+      throw new Error('Could not retrieve Nango credentials');
+    }
+
+    const rawData = credentialsRawSchema.safeParse(credentials.raw);
+
+    if (!rawData.success) {
+      throw new NonRetriableError(`Nango credentials.raw is invalid`);
+    }
 
     await deleteSalesforceUser({
       userId,
-      accessToken,
-      instanceUrl,
+      accessToken: credentials.access_token,
+      instanceUrl: credentials.raw.instance_url as string,
     });
   }
 );
