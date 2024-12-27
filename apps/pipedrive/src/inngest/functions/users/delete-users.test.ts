8c2cede5eb5f93729ae1137e6231bfa44a7cf447
mbliakharskyi <mbliakharskyi@gmail.com>
2024-12-27 11:39:43 +0200

    feat: add nango

---
+++ apps/pipedrive/src/inngest/functions/users/delete-users.test.ts
@@ -0,0 +1,40 @@
+import { expect, test, describe, beforeEach, vi } from 'vitest';
+import { createInngestFunctionMock } from '@elba-security/test-utils';
+import * as nangoAPI from '@/common/nango';
+import * as usersConnector from '@/connectors/pipedrive/users';
+import { deleteUser } from './delete-users';
+
+const userId = 'user-id';
+const organisationId = '00000000-0000-0000-0000-000000000001';
+const nangoConnectionId = 'nango-connection-id';
+const region = 'us';
+const apiDomain = 'test-api-domain';
+
+// Setup function mock for Inngest
+const setup = createInngestFunctionMock(deleteUser, 'pipedrive/users.delete.requested');
+
+describe('deleteUser', () => {
+  beforeEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  test('should delete users', async () => {
+    vi.spyOn(usersConnector, 'deleteUser').mockResolvedValueOnce();
+    // @ts-expect-error -- this is a mock
+    vi.spyOn(nangoAPI, 'nangoAPIClient', 'get').mockReturnValue({
+      getConnection: vi.fn().mockResolvedValue({
+        credentials: { access_token: 'access-token' },
+      }),
+    });
+    const [result] = setup({ organisationId, region, nangoConnectionId, userId });
+
+    await expect(result).resolves.toStrictEqual(undefined);
+
+    expect(usersConnector.deleteUser).toBeCalledTimes(1);
+    expect(usersConnector.deleteUser).toBeCalledWith({
+      userId,
+      accessToken: 'access-token',
+      apiDomain,
+    });
+  });
+});
