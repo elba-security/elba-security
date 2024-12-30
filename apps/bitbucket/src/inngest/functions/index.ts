83fed131649f11556c46799bc65b641a337cc1ef
Athithan <104758366+athielba@users.noreply.github.com>
2024-12-30 13:51:23 +0100

    Revert "feat(bitbucket): nango refactor (#552)"
    This reverts commit 501827e9a3425e47a8cd769a3c7c98b638269815.

--- apps/bitbucket/src/inngest/functions/index.ts
+++
@@ -1,5 +0,0 @@
-import { removeOrganisation } from './organisation/remove-organisation';
-import { scheduleUsersSync } from './users/schedule-users-sync';
-import { syncUsers } from './users/sync-users';
-
-export const inngestFunctions = [syncUsers, scheduleUsersSync, removeOrganisation];
--- apps/bitbucket/src/inngest/middlewares/elba-connection-error-middleware.ts
+++
@@ -1,7 +0,0 @@
-import { createElbaConnectionErrorMiddleware } from '@elba-security/inngest';
-import { mapElbaConnectionError } from '@/connectors/common/error';
-
-export const elbaConnectionErrorMiddleware = createElbaConnectionErrorMiddleware({
-  mapErrorFn: mapElbaConnectionError,
-  eventName: 'bitbucket/app.uninstalled',
-});
