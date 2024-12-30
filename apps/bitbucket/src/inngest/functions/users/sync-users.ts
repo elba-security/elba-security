501827e9a3425e47a8cd769a3c7c98b638269815
Maksym <148101061+mbliakharskyi@users.noreply.github.com>
2024-12-30 14:44:54 +0200

    feat(bitbucket): nango refactor (#552)
    ## Description
    
    Bitbucket Nango Implementation.
    
    This integration provides multiple workspaces, so need to redirect a
    user if there exists multiple workspaces. Currently didn't implemented
    the feature. I think we need a UI to select only one workspace.
    
    
    ## Additional Notes
    
    Add any other context or screenshots about the feature request here.
    
    ## Checklist:
    
    Before you create this PR, confirm all the requirements listed below by
    checking the checkboxes like this (`[x]`).
    
    - [ ] I have performed a self-review of my own code.
    - [ ] I have commented my code, particularly in hard-to-understand
    areas.
    - [ ] I have added tests to cover the new feature or fixes.

---
+++ apps/bitbucket/src/inngest/functions/users/sync-users.ts
@@ -0,0 +1,106 @@
+import type { User } from '@elba-security/sdk';
+import { NonRetriableError } from 'inngest';
+import { getWorkspaces } from '@/connectors/bitbucket/workspaces';
+import { getUsers, getAuthUser } from '@/connectors/bitbucket/users';
+import { type BitbucketUser } from '@/connectors/bitbucket/users';
+import { inngest } from '@/inngest/client';
+import { createElbaOrganisationClient } from '@/connectors/elba/client';
+import { nangoAPIClient } from '@/common/nango';
+
+const formatElbaUser = ({
+  user,
+  authUserId,
+}: {
+  user: BitbucketUser;
+  authUserId: string;
+}): User => ({
+  id: user.user.uuid,
+  displayName: user.user.display_name,
+  additionalEmails: [],
+  isSuspendable: authUserId !== user.user.uuid,
+  url: `https://bitbucket.org/${user.workspace.slug}/workspace/settings/user-directory`,
+});
+
+export const syncUsers = inngest.createFunction(
+  {
+    id: 'bitbucket-sync-users',
+    priority: {
+      run: 'event.data.isFirstSync ? 600 : 0',
+    },
+    concurrency: {
+      key: 'event.data.organisationId',
+      limit: 1,
+    },
+    retries: 5,
+    cancelOn: [
+      {
+        event: 'bitbucket/app.uninstalled',
+        match: 'data.organisationId',
+      },
+      {
+        event: 'bitbucket/app.installed',
+        match: 'data.organisationId',
+      },
+    ],
+  },
+  { event: 'bitbucket/users.sync.requested' },
+  async ({ event, step, logger }) => {
+    const { organisationId, nangoConnectionId, region, syncStartedAt, page } = event.data;
+
+    const elba = createElbaOrganisationClient({
+      organisationId,
+      region,
+    });
+
+    const nextPage = await step.run('list-users', async () => {
+      const { credentials } = await nangoAPIClient.getConnection(nangoConnectionId);
+      if (!('access_token' in credentials) || typeof credentials.access_token !== 'string') {
+        throw new NonRetriableError('Could not retrieve Nango credentials');
+      }
+      const workspaceIds = await getWorkspaces(credentials.access_token);
+      const { uuid: authUserId } = await getAuthUser(credentials.access_token);
+
+      const result = await getUsers({
+        accessToken: credentials.access_token,
+        workspaceId: workspaceIds[0].uuid,
+        page,
+      });
+
+      const users = result.validUsers.map((user) => formatElbaUser({ user, authUserId }));
+
+      if (result.invalidUsers.length > 0) {
+        logger.warn('Retrieved users contains invalid data', {
+          organisationId,
+          invalidUsers: result.invalidUsers,
+        });
+      }
+
+      if (users.length > 0) {
+        await elba.users.update({ users });
+      }
+
+      return result.nextPage;
+    });
+
+    if (nextPage) {
+      await step.sendEvent('sync-users', {
+        name: 'bitbucket/users.sync.requested',
+        data: {
+          ...event.data,
+          page: nextPage,
+        },
+      });
+      return {
+        status: 'ongoing',
+      };
+    }
+
+    await step.run('finalize', () =>
+      elba.users.delete({ syncedBefore: new Date(syncStartedAt).toISOString() })
+    );
+
+    return {
+      status: 'completed',
+    };
+  }
+);
