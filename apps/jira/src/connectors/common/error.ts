d3682c57743909ca40627494a8879c83eb3e9abc
Maksym <148101061+mbliakharskyi@users.noreply.github.com>
2025-01-06 17:11:30 +0200

    feat(jira): nango refactor (#548)
    ## Description
    
    Jira Nango Implementation
    
    1. Jira provides BASIC authentication.
    2. Added two schemas:
    - `nangoConnectionConfigSchema` 
    - `nangoCredentialsSchema`
    
    ## Additional Notes
    
    Add any other context or screenshots about the feature request here.
    
    ## Checklist:
    
    Before you create this PR, confirm all the requirements listed below by
    checking the checkboxes like this (`[x]`).
    
    - [ ] I have performed a self-review of my own code.
    - [ ] I have commented my code, particularly in hard-to-understand
    areas.
    - [ ] I have added tests to cover the new feature or fixes.

--- apps/jira/src/connectors/common/error.ts
+++ apps/jira/src/connectors/common/error.ts
@@ -1,3 +1,6 @@
+import { type MapConnectionErrorFn } from '@elba-security/inngest';
+import { NangoConnectionError } from '@elba-security/nango';
+
 type JiraErrorOptions = { response?: Response };
 
 export class JiraError extends Error {
@@ -6,5 +9,22 @@ export class JiraError extends Error {
   constructor(message: string, { response }: JiraErrorOptions = {}) {
     super(message);
     this.response = response;
+    this.name = 'JiraError';
   }
 }
+
+export class JiraNotAdminError extends JiraError {}
+
+export const mapElbaConnectionError: MapConnectionErrorFn = (error) => {
+  if (error instanceof NangoConnectionError && error.response.status === 404) {
+    return 'unauthorized';
+  }
+  if (error instanceof JiraError && error.response?.status === 401) {
+    return 'unauthorized';
+  }
+  if (error instanceof JiraNotAdminError) {
+    return 'not_admin';
+  }
+
+  return null;
+};
--- apps/jira/src/inngest/functions/users/schedule-users-sync.test.ts
+++ apps/jira/src/inngest/functions/users/schedule-users-sync.test.ts
@@ -1,21 +1,11 @@
 import { expect, test, describe, beforeAll, vi, afterAll } from 'vitest';
-import { createInngestFunctionMock } from '@elba-security/test-utils';
-import { db } from '@/database/client';
-import { organisationsTable } from '@/database/schema';
+import { createInngestFunctionMock, spyOnElba } from '@elba-security/test-utils';
+import { NonRetriableError } from 'inngest';
 import { scheduleUsersSync } from './schedule-users-sync';
 
 const now = Date.now();
 
 const setup = createInngestFunctionMock(scheduleUsersSync);
-
-export const organisations = Array.from({ length: 5 }, (_, i) => ({
-  id: `00000000-0000-0000-0000-00000000000${i}`,
-  apiToken: `test-api-token${i}`,
-  domain: 'test-domain',
-  email: 'test-email',
-  region: `us`,
-  authUserId: 'test-authUser-id',
-}));
 
 describe('schedule-users-sync', () => {
   beforeAll(() => {
@@ -27,30 +17,113 @@ describe('schedule-users-sync', () => {
   });
 
   test('should not schedule any jobs when there are no organisations', async () => {
+    const elba = spyOnElba();
+    elba.mockImplementation(() => ({
+      // @ts-expect-error -- this is a mock
+      organisations: {
+        list: vi.fn().mockResolvedValue({ organisations: [] }),
+      },
+    }));
     const [result, { step }] = setup();
+
     await expect(result).resolves.toStrictEqual({ organisations: [] });
     expect(step.sendEvent).toBeCalledTimes(0);
   });
 
   test('should schedule jobs when there are organisations', async () => {
-    await db.insert(organisationsTable).values(organisations);
+    const elba = spyOnElba();
+    const regionOrganisations = new Map(
+      ['eu', 'us'].map((region) => [
+        region,
+        [{ id: `organisation-id-${region}`, nangoConnectionId: `nango-connection-id-${region}` }],
+      ])
+    );
+    elba.mockImplementation(({ region }) => ({
+      // @ts-expect-error -- this is a mock
+      organisations: {
+        list: vi.fn().mockResolvedValue({
+          organisations: regionOrganisations.get(region) || [],
+        }),
+      },
+    }));
+
     const [result, { step }] = setup();
 
     await expect(result).resolves.toStrictEqual({
-      organisations: organisations.map(({ id }) => ({ id })),
+      organisations: [...regionOrganisations].flatMap(([region, organisations]) =>
+        organisations.map((organisation) => ({
+          organisationId: organisation.id,
+          nangoConnectionId: organisation.nangoConnectionId,
+          region,
+        }))
+      ),
     });
+
     expect(step.sendEvent).toBeCalledTimes(1);
     expect(step.sendEvent).toBeCalledWith(
       'synchronize-users',
-      organisations.map(({ id }) => ({
-        name: 'jira/users.sync.requested',
-        data: {
-          organisationId: id,
-          syncStartedAt: now,
-          isFirstSync: false,
-          page: null,
-        },
-      }))
+      [...regionOrganisations].flatMap(([region, organisations]) =>
+        organisations.map(({ id: organisationId, nangoConnectionId }) => ({
+          name: 'jira/users.sync.requested',
+          data: {
+            region,
+            organisationId,
+            nangoConnectionId,
+            syncStartedAt: now,
+            isFirstSync: false,
+            page: null,
+          },
+        }))
+      )
+    );
+  });
+
+  test('should schedule jobs and throw an error when there organisation nango connection id is missing', async () => {
+    const elba = spyOnElba();
+    const regionOrganisations = new Map(
+      ['eu', 'us'].map((region) => [
+        region,
+        [
+          {
+            id: `organisation-id-${region}`,
+            nangoConnectionId: region === 'us' ? null : `nango-connection-id-${region}`,
+          },
+        ],
+      ])
+    );
+    elba.mockImplementation(({ region }) => ({
+      // @ts-expect-error -- this is a mock
+      organisations: {
+        list: vi.fn().mockResolvedValue({
+          organisations: regionOrganisations.get(region) || [],
+        }),
+      },
+    }));
+
+    const [result, { step }] = setup();
+
+    await expect(result).rejects.toStrictEqual(
+      new NonRetriableError('Failed to schedule users sync due to missing nango connection ID')
+    );
+
+    expect(step.sendEvent).toBeCalledTimes(1);
+    expect(step.sendEvent).toBeCalledWith(
+      'synchronize-users',
+      [...regionOrganisations]
+        .filter(([region]) => region !== 'us')
+        .flatMap(([region, organisations]) =>
+          organisations.map(({ id: organisationId, nangoConnectionId }) => ({
+            name: 'jira/users.sync.requested',
+            data: {
+              region,
+              organisationId,
+              nangoConnectionId,
+              syncStartedAt: now,
+              isFirstSync: false,
+              page: null,
+            },
+          }))
+        )
     );
   });
 });
