8f45e3ead92d07aef2caf0caec0fdf6fae6efcbb
Athithan <104758366+athielba@users.noreply.github.com>
2025-01-08 16:10:31 +0100

    feat(zoom): nango refactor (#557)
    ## Description
    Nango integration
    
    ## Additional Notes
    
    Add any other context or screenshots about the feature request here.
    
    ## Checklist:
    
    Before you create this PR, confirm all the requirements listed below by
    checking the checkboxes like this (`[x]`).
    
    - [ ] I have performed a self-review of my own code.
    - [ ] I have commented my code, particularly in hard-to-understand
    areas.
    - [ ] I have added tests to cover the new feature or fixes.
    
    ## Description
    
    Please include a summary of the changes and issues addressed in this PR.
    Also, explain the motivation and context of each update. List any
    dependencies or prerequisites needed for implementing this change.
    
    ## Additional Notes
    
    Add any other context or screenshots about the feature request here.
    
    ## Checklist:
    
    Before you create this PR, confirm all the requirements listed below by
    checking the checkboxes like this (`[x]`).
    
    - [ ] I have performed a self-review of my own code.
    - [ ] I have commented my code, particularly in hard-to-understand
    areas.
    - [ ] I have added tests to cover the new feature or fixes.
    
    ---------
    
    Co-authored-by: Maksym <148101061+mbliakharskyi@users.noreply.github.com>
    Co-authored-by: Guillaume PROQUIN <guillaume.proquin@epitech.eu>

---
+++ apps/zoom/src/app/api/webhooks/elba/installation/validate/service.ts
@@ -0,0 +1,71 @@
+import { serializeLogObject } from '@elba-security/logger/src/serialize';
+import { logger } from '@elba-security/logger';
+import { nangoAPIClient } from '@/common/nango';
+import { getAuthUser } from '@/connectors/zoom/users';
+import { createElbaOrganisationClient } from '@/connectors/elba/client';
+import { inngest } from '@/inngest/client';
+import { mapElbaConnectionError } from '@/connectors/common/error';
+
+export const validateSourceInstallation = async ({
+  organisationId,
+  region,
+  nangoConnectionId,
+}: {
+  organisationId: string;
+  region: string;
+  nangoConnectionId: string;
+}) => {
+  const elba = createElbaOrganisationClient({
+    organisationId,
+    region,
+  });
+  try {
+    const { credentials } = await nangoAPIClient.getConnection(nangoConnectionId);
+    if (!('access_token' in credentials) || typeof credentials.access_token !== 'string') {
+      throw new Error('Could not retrieve Nango credentials');
+    }
+
+    await getAuthUser(credentials.access_token);
+
+    await elba.connectionStatus.update({
+      errorType: null,
+    });
+
+    await inngest.send([
+      {
+        name: 'zoom/app.installed',
+        data: {
+          organisationId,
+        },
+      },
+      {
+        name: 'zoom/users.sync.requested',
+        data: {
+          organisationId,
+          region,
+          nangoConnectionId,
+          isFirstSync: true,
+          syncStartedAt: Date.now(),
+          page: null,
+        },
+      },
+    ]);
+
+    return { message: 'Source installation validated' };
+  } catch (error) {
+    logger.error('Failed to validate installation', {
+      organisationId,
+      region,
+      nangoConnectionId,
+      error,
+    });
+    const errorType = mapElbaConnectionError(error);
+
+    await elba.connectionStatus.update({
+      errorType: errorType || 'unknown',
+      errorMetadata: serializeLogObject(error),
+    });
+
+    return { message: 'Source installation validation failed' };
+  }
+};
