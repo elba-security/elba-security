d306fa862f10826c5624a76ce7d7d0c139867f41
mbliakharskyi <mbliakharskyi@gmail.com>
2025-01-10 09:02:47 +0200

    feat: add nango

---
+++ apps/sendgrid/src/common/env.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod';
+
+const zEnvInt = () => z.coerce.number().int().positive();
+
+export const env = z
+  .object({
+    ELBA_API_KEY: z.string().min(1),
+    ELBA_API_BASE_URL: z.string().url(),
+    ELBA_SOURCE_ID: z.string().uuid(),
+    ELBA_WEBHOOK_SECRET: z.string().min(1),
+    SENDGRID_API_BASE_URL: z.string().min(1).default('https://api.sendgrid.com'),
+    SENDGRID_USERS_SYNC_CRON: z.string().default('0 0 * * *'),
+    SENDGRID_USERS_SYNC_CONCURRENCY: zEnvInt().default(1),
+    SENDGRID_DELETE_USER_CONCURRENCY: zEnvInt().default(1),
+    SENDGRID_USERS_SYNC_BATCH_SIZE: zEnvInt().default(300), // Default=500, MAX=500
+    NANGO_INTEGRATION_ID: z.string().min(1),
+    NANGO_SECRET_KEY: z.string().min(1),
+  })
+  .parse(process.env);
---
+++ apps/sendgrid/src/inngest/middlewares/rate-limit-middleware.ts
@@ -0,0 +1,44 @@
+import { InngestMiddleware, RetryAfterError } from 'inngest';
+import { SendgridError } from '@/connectors/common/error';
+
+export const rateLimitMiddleware = new InngestMiddleware({
+  name: 'rate-limit',
+  init: () => {
+    return {
+      onFunctionRun: ({ fn }) => {
+        return {
+          transformOutput: (ctx) => {
+            const {
+              result: { error, ...result },
+              ...context
+            } = ctx;
+
+            if (!(error instanceof SendgridError)) {
+              return;
+            }
+
+            if (error.response?.status === 429) {
+              const resetTimestamp = error.response.headers.get('x-ratelimit-reset'); // in seconds, Request can be made after this time
+              const currentTime = Math.floor(Date.now() / 1000);
+              const retryAfter = resetTimestamp ? parseInt(resetTimestamp, 10) - currentTime : 60;
+
+              return {
+                ...context,
+                result: {
+                  ...result,
+                  error: new RetryAfterError(
+                    `Rate limit exceeded for '${fn.name}'. Retry after ${retryAfter} seconds.`,
+                    `${retryAfter}s`,
+                    {
+                      cause: error,
+                    }
+                  ),
+                },
+              };
+            }
+          },
+        };
+      },
+    };
+  },
+});
