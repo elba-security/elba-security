cfe75c7257b3be157b1c5149a52eabe679752332
Maksym <148101061+mbliakharskyi@users.noreply.github.com>
2025-01-14 19:29:34 +0200

    feat(brevo): sync users, nango implementation (#568)
    ## Description
    
    Brevo Integration
    1. Nango implementation (API key authentication)
    2. Sync users
    3. User deletion feature not implemented because their API has some
    issue.
    4. The endpoint doesn't provide pagination. Currently list all users at
    a once.
    
    ## Additional Notes
    
    Add any other context or screenshots about the feature request here.
    
    ## Checklist:
    
    Before you create this PR, confirm all the requirements listed below by
    checking the checkboxes like this (`[x]`).
    
    - [x] I have performed a self-review of my own code.
    - [x] I have commented my code, particularly in hard-to-understand
    areas.
    - [x] I have added tests to cover the new feature or fixes.

---
+++ apps/brevo/src/inngest/functions/users/sync-users.ts
@@ -0,0 +1,82 @@
+import type { User } from '@elba-security/sdk';
+import { logger } from '@elba-security/logger';
+import { getUsers } from '@/connectors/brevo/users';
+import { inngest } from '@/inngest/client';
+import { type BrevoUser } from '@/connectors/brevo/users';
+import { createElbaOrganisationClient } from '@/connectors/elba/client';
+import { nangoCredentialsSchema } from '@/connectors/common/nango';
+import { nangoAPIClient } from '@/common/nango';
+
+const formatElbaUser = (user: BrevoUser): User => ({
+  id: user.id,
+  displayName: user.email,
+  email: user.email,
+  additionalEmails: [],
+  url: `https://app.brevo.com/user/member`,
+  isSuspendable: !user.is_owner,
+});
+
+export const synchronizeUsers = inngest.createFunction(
+  {
+    id: 'brevo-synchronize-users',
+    priority: {
+      run: 'event.data.isFirstSync ? 600 : 0',
+    },
+    concurrency: {
+      key: 'event.data.organisationId',
+      limit: 1,
+    },
+    cancelOn: [
+      {
+        event: 'brevo/app.uninstalled',
+        match: 'data.organisationId',
+      },
+      {
+        event: 'brevo/app.installed',
+        match: 'data.organisationId',
+      },
+    ],
+    retries: 3,
+  },
+  { event: 'brevo/users.sync.requested' },
+  async ({ event, step }) => {
+    const { organisationId, nangoConnectionId, region, syncStartedAt } = event.data;
+
+    const elba = createElbaOrganisationClient({
+      organisationId,
+      region,
+    });
+
+    const { credentials } = await nangoAPIClient.getConnection(nangoConnectionId);
+    const nangoCredentialsResult = nangoCredentialsSchema.safeParse(credentials);
+    if (!nangoCredentialsResult.success) {
+      throw new Error('Could not retrieve Nango credentials');
+    }
+
+    const result = await getUsers(nangoCredentialsResult.data.apiKey);
+
+    const users = result.validUsers
+      .filter(({ status }) => status === 'active')
+      .map((user) => formatElbaUser(user));
+
+    if (result.invalidUsers.length > 0) {
+      logger.warn('Retrieved users contains invalid data', {
+        organisationId,
+        invalidUsers: result.invalidUsers,
+      });
+    }
+
+    if (users.length > 0) {
+      await elba.users.update({ users });
+    }
+
+    // delete the elba users that has been sent before this sync
+    await step.run('finalize', () =>
+      elba.users.delete({ syncedBefore: new Date(syncStartedAt).toISOString() })
+    );
+
+    return {
+      status: 'completed',
+    };
+  }
+);
