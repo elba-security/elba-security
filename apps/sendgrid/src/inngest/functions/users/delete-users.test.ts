67912356eb4ad57ac64992d954a02b6f662a1bd1
mbliakharskyi <mbliakharskyi@gmail.com>
2025-02-14 09:36:48 +0200

    feat: add nango refactor

---
+++ apps/sendgrid/src/inngest/functions/users/delete-users.test.ts
@@ -0,0 +1,43 @@
+import { expect, test, describe, beforeEach, vi } from 'vitest';
+import { createInngestFunctionMock } from '@elba-security/test-utils';
+import * as nangoAPI from '@/common/nango';
+import * as usersConnector from '@/connectors/sendgrid/users';
+import { deleteUser } from './delete-users';
+
+const organisationId = '00000000-0000-0000-0000-000000000001';
+const nangoConnectionId = 'nango-connection-id';
+const region = 'us';
+const userId = 'user-id-1';
+const apiKey = 'test-api-key';
+
+const setup = createInngestFunctionMock(deleteUser, 'sendgrid/users.delete.requested');
+
+describe('deleteUser', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  beforeEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  test('should delete users', async () => {
+    vi.spyOn(usersConnector, 'deleteUser').mockResolvedValueOnce();
+    // @ts-expect-error -- this is a mock
+    vi.spyOn(nangoAPI, 'nangoAPIClient', 'get').mockReturnValue({
+      getConnection: vi.fn().mockResolvedValue({
+        credentials: { apiKey: 'test-api-key' },
+      }),
+    });
+
+    const [result] = setup({ organisationId, region, nangoConnectionId, userId });
+
+    await expect(result).resolves.toStrictEqual(undefined);
+
+    expect(usersConnector.deleteUser).toBeCalledTimes(1);
+    expect(usersConnector.deleteUser).toBeCalledWith({
+      userId,
+      apiKey,
+    });
+  });
+});
