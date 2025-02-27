1d26b622d6e1648f19d09eeeb097d919e051bd4d
Maksym <148101061+mbliakharskyi@users.noreply.github.com>
2025-02-27 20:27:17 +0200

    feat(pipedrive): nango implementation (#597)
    ## Description
    
    Please include a summary of the changes and issues addressed in this PR.
    Also, explain the motivation and context of each update. List any
    dependencies or prerequisites needed for implementing this change.
    
    ## Additional Notes
    
    Add any other context or screenshots about the feature request here.
    
    ## Checklist:
    
    Before you create this PR, confirm all the requirements listed below by
    checking the checkboxes like this (`[x]`).
    
    - [ ] I have performed a self-review of my own code.
    - [ ] I have commented my code, particularly in hard-to-understand
    areas.
    - [ ] I have added tests to cover the new feature or fixes.

---
+++ apps/pipedrive/src/inngest/functions/users/schedule-users-sync.ts
@@ -0,0 +1,70 @@
+import { elbaRegions } from '@elba-security/sdk';
+import { logger } from '@elba-security/logger';
+import { NonRetriableError } from 'inngest';
+import { env } from '@/common/env';
+import { createElbaGlobalClient } from '@/connectors/elba/client';
+import { inngest } from '../../client';
+
+export const scheduleUsersSync = inngest.createFunction(
+  {
+    id: 'pipedrive-schedule-users-syncs',
+    retries: 5,
+  },
+  { cron: env.PIPEDRIVE_USERS_SYNC_CRON },
+  async ({ step }) => {
+    const regionOrganisations = await Promise.all(
+      elbaRegions.map((region) =>
+        step.run(`get-organisations-${region}`, async () => {
+          const elba = createElbaGlobalClient(region);
+          const result = await elba.organisations.list();
+          return result.organisations.map(({ id: organisationId, nangoConnectionId }) => ({
+            organisationId,
+            nangoConnectionId,
+            region,
+          }));
+        })
+      )
+    );
+
+    const invalidOrganisations: { organisationId: string; region: string }[] = [];
+    const organisations: {
+      organisationId: string;
+      region: string;
+      nangoConnectionId: string;
+    }[] = [];
+    for (const { organisationId, region, nangoConnectionId } of regionOrganisations.flat()) {
+      if (!nangoConnectionId) {
+        invalidOrganisations.push({ organisationId, region });
+      } else {
+        organisations.push({ organisationId, region, nangoConnectionId });
+      }
+    }
+
+    if (organisations.length) {
+      await step.sendEvent(
+        'synchronize-users',
+        organisations.map(({ organisationId, nangoConnectionId, region }) => ({
+          name: 'pipedrive/users.sync.requested',
+          data: {
+            isFirstSync: false,
+            organisationId,
+            nangoConnectionId,
+            region,
+            syncStartedAt: Date.now(),
+            page: null,
+          },
+        }))
+      );
+    }
+
+    if (invalidOrganisations.length) {
+      logger.error('Failed to schedule users sync due to missing nango connection ID', {
+        invalidOrganisations,
+      });
+      throw new NonRetriableError(
+        'Failed to schedule users sync due to missing nango connection ID'
+      );
+    }
+    return { organisations };
+  }
+);
