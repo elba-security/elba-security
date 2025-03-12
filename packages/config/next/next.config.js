dea25fbfe1c7a9f5b83dd90c34e0ec600a2ba433
Guillaume PROQUIN <guillaume.proquin@epitech.eu>
2025-03-12 11:47:39 +0100

    chore: speed up vercel deployment

--- packages/config/next/next.config.js
+++ packages/config/next/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   transpilePackages: ['@elba-security/design-system'],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
