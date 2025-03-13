18b58b9b764466385d62bee73be5ed8d019f94af
Guillaume PROQUIN <guillaume.proquin@epitech.eu>
2025-03-13 18:08:45 +0100

    fix(sentry): token refresh

--- apps/sentry/src/inngest/functions/index.ts
+++ apps/sentry/src/inngest/functions/index.ts
@@ -3,11 +3,13 @@ import { syncUsers } from './users/sync-users';
 import { scheduleUsersSync } from './users/schedule-users-sync';
 import { deleteUser } from './users/delete-users';
 import { refreshToken } from './token/refresh-token';
+import { scheduleTokenRefresh } from './token/schedule-token-refresh';
 
 export const inngestFunctions = [
   deleteUser,
   refreshToken,
   removeOrganisation,
+  scheduleTokenRefresh,
   scheduleUsersSync,
   syncUsers,
 ];
