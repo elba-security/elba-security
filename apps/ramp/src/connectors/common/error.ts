5ed21c03ebec676e4ea4c0c681906944221853ba
mbliakharskyi <mbliakharskyi@gmail.com>
2025-04-16 08:03:32 +0300

    feat: auth, list users, pagination and rate limit

---
+++ apps/ramp/src/connectors/common/error.ts
@@ -0,0 +1,30 @@
+import { type MapConnectionErrorFn } from '@elba-security/inngest';
+import { NangoConnectionError } from '@elba-security/nango';
+
+type RampErrorOptions = { response?: Response };
+
+export class RampError extends Error {
+  response?: Response;
+
+  constructor(message: string, { response }: RampErrorOptions = {}) {
+    super(message);
+    this.response = response;
+    this.name = 'RampError';
+  }
+}
+
+export class RampNotAdminError extends RampError {}
+
+export const mapElbaConnectionError: MapConnectionErrorFn = (error) => {
+  if (error instanceof NangoConnectionError && error.response.status === 404) {
+    return 'unauthorized';
+  }
+  if (error instanceof RampError && error.response?.status === 401) {
+    return 'unauthorized';
+  }
+  if (error instanceof RampNotAdminError) {
+    return 'not_admin';
+  }
+
+  return null;
+};
