d3a9cd229c0073228bb4c1d1ffa3184844b27482
Guillaume PROQUIN <guillaume.proquin@epitech.eu>
2025-05-02 16:07:45 +0700

    chore: update ramp & bill

--- apps/bill/src/connectors/common/error.ts
+++ apps/bill/src/connectors/common/error.ts
@@ -1,5 +1,5 @@
 import { type MapConnectionErrorFn } from '@elba-security/inngest';
-import { NangoConnectionError } from '@elba-security/nango';
+import { IntegrationConnectionError } from '@elba-security/common';
 
 type BillErrorOptions = { response?: Response };
 
@@ -14,8 +14,8 @@ export class BillError extends Error {
 }
 
 export const mapElbaConnectionError: MapConnectionErrorFn = (error) => {
-  if (error instanceof NangoConnectionError && error.response.status === 404) {
-    return 'unauthorized';
+  if (error instanceof IntegrationConnectionError) {
+    return error.type;
   }
   if (error instanceof BillError && error.response?.status === 401) {
     return 'unauthorized';
