8a022ff5e5f680db94d36b94bb22d17fdaaaebbd
mbliakharskyi <mbliakharskyi@gmail.com>
2025-05-04 16:50:20 +0300

    refactor: remove a variable

--- apps/discourse/src/common/env.ts
+++ apps/discourse/src/common/env.ts
@@ -9,7 +9,6 @@ export const env = z
     ELBA_SOURCE_ID: z.string().uuid(),
     ELBA_WEBHOOK_SECRET: z.string().min(1),
     DISCOURSE_DELETE_USER_CONCURRENCY: zEnvInt().default(5),
-    DISCOURSE_USERS_SYNC_BATCH_SIZE: zEnvInt().default(20),
     DISCOURSE_USERS_SYNC_CRON: z.string().default('0 0 * * *'),
     NANGO_INTEGRATION_ID: z.string().min(1),
     NANGO_SECRET_KEY: z.string().min(1),
