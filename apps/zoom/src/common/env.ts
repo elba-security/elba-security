560e4b3cda0ed32b77fcae2d2acd3c9b2e41c6a4
Guillaume PROQUIN <proquig@users.noreply.github.com>
2025-05-27 06:06:32 +0200

    refactor(zoom): elba communication using inngest (#630)
    ## Description
    
    This refactors the Zoom integration to use the new architecture to
    communicate with elba using Inngest
    
    ## Additional Notes
    
    Add any other context or screenshots about the feature request here.
    
    ## Checklist:
    
    Before you create this PR, confirm all the requirements listed below by
    checking the checkboxes like this (`[x]`).
    
    - [x] I have performed a self-review of my own code.
    - [x] I have commented my code, particularly in hard-to-understand
    areas.
    - [x] I have added tests to cover the new feature or fixes.

--- apps/zoom/src/common/env.ts
+++ apps/zoom/src/common/env.ts
@@ -4,15 +4,16 @@ const zEnvInt = () => z.coerce.number().int().positive();
 
 export const env = z
   .object({
-    ELBA_API_KEY: z.string().min(1),
-    ELBA_API_BASE_URL: z.string().url(),
+    // Elba configuration
     ELBA_SOURCE_ID: z.string().uuid(),
-    ELBA_WEBHOOK_SECRET: z.string().min(1),
-    ZOOM_API_BASE_URL: z.string().url().default('https://api.zoom.us/v2'),
-    ZOOM_DELETE_USER_CONCURRENCY: zEnvInt().default(5),
-    ZOOM_USERS_SYNC_CRON: z.string().default('0 0 * * *'),
-    ZOOM_USERS_SYNC_BATCH_SIZE: zEnvInt().default(2),
+
+    // Nango configuration
     NANGO_INTEGRATION_ID: z.string().min(1),
     NANGO_SECRET_KEY: z.string().min(1),
+
+    // Source configuration
+    ZOOM_API_BASE_URL: z.string().url(),
+    ZOOM_USERS_SYNC_CRON: z.string().default('0 0 * * *'),
+    ZOOM_USERS_SYNC_BATCH_SIZE: zEnvInt().default(50),
   })
   .parse(process.env);
