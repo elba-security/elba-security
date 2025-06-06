1a291587fab4aa5bba7b8f35e24f9df045953214
Yurii Finohenov <yurii.finohenov@freshcodeit.com>
2025-06-06 06:59:47 +0000

    feat(outlook): add installation and user sync [E-2534]

---
+++ apps/outlook/src/app/install/route.ts
@@ -0,0 +1,34 @@
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+import { type NextRequest } from 'next/server';
+import { logger } from '@elba-security/logger';
+import { ElbaInstallRedirectResponse } from '@elba-security/nextjs';
+import { env } from '@/common/env/server';
+
+export const preferredRegion = 'fra1';
+export const runtime = 'edge';
+export const dynamic = 'force-dynamic';
+
+export function GET(request: NextRequest) {
+  const organisationId = request.nextUrl.searchParams.get('organisation_id');
+  const region = request.nextUrl.searchParams.get('region');
+
+  if (!organisationId || !region) {
+    logger.warn('Could not redirect user to Microsoft app install url');
+    return new ElbaInstallRedirectResponse({
+      region,
+      sourceId: env.ELBA_SOURCE_ID,
+      baseUrl: env.ELBA_REDIRECT_URL,
+      error: 'unauthorized',
+    });
+  }
+
+  cookies().set('organisationId', organisationId);
+  cookies().set('region', region);
+
+  const url = new URL(env.OUTLOOK_INSTALL_URL);
+  url.searchParams.append('client_id', env.OUTLOOK_AUTH_CLIENT_ID);
+  url.searchParams.append('redirect_uri', env.OUTLOOK_AUTH_REDIRECT_URI);
+
+  redirect(url.toString());
+}
---
+++ apps/outlook/src/inngest/functions/users/sync.ts
@@ -0,0 +1,99 @@
+import type { User } from '@elba-security/sdk';
+import { env } from '@/common/env/server';
+import { inngest } from '@/inngest/client';
+import { getUsers } from '@/connectors/microsoft/user';
+import { decrypt } from '@/common/crypto';
+import { getElbaClient } from '@/connectors/elba/client';
+import type { MicrosoftUser } from '@/connectors/microsoft/user';
+import { getOrganisation } from '../common/get-organisation';
+
+export type SyncUsersEvents = {
+  'outlook/users.sync.requested': SyncUsersRequested;
+};
+
+type SyncUsersRequested = {
+  data: {
+    organisationId: string;
+    isFirstSync: boolean;
+    syncStartedAt: number;
+    skipToken: string | null;
+  };
+};
+
+const formatElbaUser = (user: MicrosoftUser): User => ({
+  id: user.id,
+  email: user.mail || undefined,
+  displayName: user.displayName || user.userPrincipalName,
+  additionalEmails: [],
+});
+
+export const syncUsers = inngest.createFunction(
+  {
+    id: 'outlook-sync-users',
+    priority: {
+      run: 'event.data.isFirstSync ? 600 : 0',
+    },
+    concurrency: {
+      key: 'event.data.organisationId',
+      limit: 1,
+    },
+    cancelOn: [
+      {
+        event: 'outlook/common.organisation.inserted',
+        match: 'data.organisationId',
+      },
+    ],
+    retries: env.USERS_SYNC_MAX_RETRY,
+  },
+  { event: 'outlook/users.sync.requested' },
+  async ({ event, step, logger }) => {
+    const { organisationId, skipToken, syncStartedAt } = event.data;
+
+    const { region, token, tenantId } = await step.invoke('get-organisation', {
+      function: getOrganisation,
+      data: { organisationId, columns: ['region', 'token', 'tenantId'] },
+    });
+
+    const elba = getElbaClient({ organisationId, region });
+
+    const nextSkipToken = await step.run('paginate', async () => {
+      const result = await getUsers({
+        token: await decrypt(token),
+        tenantId,
+        skipToken,
+      });
+
+      if (result.invalidUsers.length > 0) {
+        logger.warn('Retrieved users contains invalid data', {
+          organisationId,
+          tenantId,
+          invalidUsers: result.invalidUsers,
+        });
+      }
+
+      await elba.users.update({
+        users: result.validUsers.map(formatElbaUser),
+      });
+
+      return result.nextSkipToken;
+    });
+
+    if (nextSkipToken) {
+      await step.sendEvent('sync-next-users-page', {
+        name: 'outlook/users.sync.requested',
+        data: {
+          ...event.data,
+          skipToken: nextSkipToken,
+        },
+      });
+
+      return {
+        status: 'ongoing',
+      };
+    }
+    await elba.users.delete({ syncedBefore: new Date(syncStartedAt).toISOString() });
+    return {
+      status: 'completed',
+    };
+  }
+);
