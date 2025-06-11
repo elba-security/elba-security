ff86a3d2919a07e1e65303b994ac99a48ee8ecb4
cracadumi <antoine.n2c@gmail.com>
2025-06-11 11:07:49 +0200

    chore: refacto hubspot integration to use inngest

--- apps/hubspot/src/inngest/functions/index.ts
+++
@@ -1,6 +0,0 @@
-import { removeOrganisation } from './organisations/remove-organisation';
-import { syncUsers } from './users/sync-users';
-import { scheduleUsersSync } from './users/schedule-users-sync';
-import { deleteUser } from './users/delete-user';
-
-export const inngestFunctions = [removeOrganisation, scheduleUsersSync, syncUsers, deleteUser];
